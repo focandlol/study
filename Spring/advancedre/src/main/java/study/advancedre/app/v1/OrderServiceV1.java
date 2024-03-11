52936575b9a6d65bf0ba30e21c7fe94eb19f04c0
focandlol <focandlol@naver.com>
2024-03-11 13:39:07 +0900

    Apply log v1

--- Spring/advancedre/src/main/java/study/advancedre/app/v1/OrderServiceV1.java
+++ Spring/advancedre/src/main/java/study/advancedre/app/v1/OrderServiceV1.java
@@ -2,14 +2,23 @@ package study.advancedre.app.v1;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import study.advancedre.trace.TraceStatus;
+import study.advancedre.trace.hellotrace.HelloTraceV1;
 
 @Service
 @RequiredArgsConstructor
 public class OrderServiceV1 {
-
-    private final OrderRepositoryV1 orderRepositoryV0;
-
-    public void orderItem(String itemId){
-        orderRepositoryV0.save(itemId);
+    private final OrderRepositoryV1 orderRepository;
+    private final HelloTraceV1 trace;
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+            orderRepository.save(itemId);
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e;
+        }
     }
 }
