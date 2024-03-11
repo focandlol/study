1a68a2c6e20af0d9a53b3f578d9f33cb5d528d0d
focandlol <focandlol@naver.com>
2024-03-11 16:56:25 +0900

    Apply threadLocal sync

--- Spring/advancedre/src/main/java/study/advancedre/LogTraceConfig.java
+++ Spring/advancedre/src/main/java/study/advancedre/LogTraceConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import study.advancedre.trace.logtrace.FieldLogTrace;
 import study.advancedre.trace.logtrace.LogTrace;
+import study.advancedre.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace(){
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
