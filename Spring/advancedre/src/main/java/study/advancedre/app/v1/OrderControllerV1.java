283acebf1e224dec23247722f733dbf6f1105268
focandlol <focandlol@naver.com>
2024-03-11 13:39:07 +0900

    Apply log v1

--- Spring/advancedre/src/main/java/study/advancedre/app/v1/OrderControllerV1.java
+++ Spring/advancedre/src/main/java/study/advancedre/app/v1/OrderControllerV1.java
@@ -3,6 +3,7 @@ package study.advancedre.app.v1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import study.advancedre.trace.TraceStatus;
 import study.advancedre.trace.hellotrace.HelloTraceV1;
 
 @RestController
@@ -13,7 +14,18 @@ public class OrderControllerV1 {
     private final HelloTraceV1 trace;
     @GetMapping("/v1/request")
     public String request(String itemId){
-        orderService.orderItem(itemId);
-        return "ok";
+
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "ok";
+        }catch(Exception e){
+            trace.exception(status,e);
+            throw e;
+        }
+
     }
 }
--- Spring/advancedre/src/main/java/study/advancedre/app/v1/OrderRepositoryV1.java
+++ Spring/advancedre/src/main/java/study/advancedre/app/v1/OrderRepositoryV1.java
@@ -2,16 +2,31 @@ package study.advancedre.app.v1;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import study.advancedre.trace.TraceStatus;
+import study.advancedre.trace.hellotrace.HelloTraceV1;
 
 @Repository
 @RequiredArgsConstructor
 public class OrderRepositoryV1 {
 
+    private final HelloTraceV1 trace;
+
     public void save(String itemId){
-        if(itemId.equals("ex")){
-            throw new IllegalStateException("예외 발생");
+
+        TraceStatus status = null;
+
+        try {
+            status = trace.begin("OrderRepository.save()");
+            if(itemId.equals("ex")){
+                throw new IllegalStateException("예외 발생");
+            }
+            sleep(1000);
+            trace.end(status);
+        }catch(Exception e){
+            trace.exception(status,e);
+            throw e;
         }
-        sleep(1000);
+
     }
 
     private void sleep(int millis){
