0a5fb5863314e67f65329d6395999198c6b65b75
focandlol <focandlol@naver.com>
2024-03-11 12:32:35 +0900

    start

---
+++ Spring/advancedre/src/main/java/study/advancedre/AdvancedreApplication.java
@@ -0,0 +1,13 @@
+package study.advancedre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedreApplication.class, args);
+	}
+
+}
---
+++ Spring/advancedre/src/test/java/study/advancedre/AdvancedreApplicationTests.java
@@ -0,0 +1,13 @@
+package study.advancedre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
