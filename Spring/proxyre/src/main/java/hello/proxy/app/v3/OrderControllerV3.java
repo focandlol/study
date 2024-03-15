2374697403ff9baed5f7b3c57c435058f59e5de3
focandlol <focandlol@naver.com>
2024-03-15 16:50:50 +0900

    Apply Spring beanPostProcessor

--- Spring/proxyre/src/main/java/hello/proxy/app/v3/OrderControllerV3.java
+++ Spring/proxyre/src/main/java/hello/proxy/app/v3/OrderControllerV3.java
@@ -1,10 +1,12 @@
 package hello.proxy.app.v3;
 
 import hello.proxy.app.v2.OrderServiceV2;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class OrderControllerV3 {
 
     private final OrderServiceV3 orderService;
@@ -16,6 +18,7 @@ public class OrderControllerV3 {
     @GetMapping("/v3/request")
     public String request(String itemId) {
         orderService.orderItem(itemId);
+        log.info("controller result 반환");
         return "ok";
 
     }
