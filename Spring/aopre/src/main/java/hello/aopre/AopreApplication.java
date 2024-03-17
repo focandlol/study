85faceab870595d87a6509be5f6b39be11f9731b
focandlol <focandlol@naver.com>
2024-03-17 10:05:35 +0900

    Separated Pointcut

---
+++ Spring/aopre/src/main/java/hello/aopre/AopreApplication.java
@@ -0,0 +1,13 @@
+package hello.aopre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopreApplication.class, args);
+	}
+
+}
---
+++ Spring/aopre/src/test/java/hello/aopre/AopTest.java
@@ -0,0 +1,47 @@
+package hello.aopre;
+
+import hello.aopre.order.OrderRepository;
+import hello.aopre.order.OrderService;
+import hello.aopre.order.aop.AspectV1;
+import hello.aopre.order.aop.AspectV2;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+//@Import(AspectV1.class)
+@Import(AspectV2.class)
+public class AopTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo(){
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success(){
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception(){
+        assertThatThrownBy(() -> orderService.orderItem("ex"))
+                .isInstanceOf(IllegalStateException.class);
+
+    }
+
+}
