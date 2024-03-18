769dd0c0050c55c08926079e59e0fbcf94172f69
focandlol <focandlol@naver.com>
2024-03-18 13:33:17 +0900

    bean

--- Spring/aopre/src/test/java/hello/aopre/pointcut/beanTest.java
+++ Spring/aopre/src/test/java/hello/aopre/pointcut/beanTest.java
@@ -1,4 +1,35 @@
 package hello.aopre.pointcut;
 
+import hello.aopre.order.OrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@SpringBootTest
+@Import(beanTest.BeanAspect.class)
 public class beanTest {
+
+    @Autowired
+    OrderService orderService;
+
+    @Test
+    void success(){
+        orderService.orderItem("itemA");
+
+    }
+
+    @Aspect
+    static class BeanAspect{
+        @Around("bean(orderService) || bean(*Repository)")
+        public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
+            log.info("[bean] {}",joinPoint.getSignature());
+            return joinPoint.proceed();
+        }
+    }
 }
