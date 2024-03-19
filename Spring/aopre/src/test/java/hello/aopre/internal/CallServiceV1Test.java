8f78fcfccf4016bad8275c06e1b681df8a652264
focandlol <focandlol@naver.com>
2024-03-19 12:24:44 +0900

    Proxy internal calls

---
+++ Spring/aopre/src/test/java/hello/aopre/internal/CallServiceV1Test.java
@@ -0,0 +1,27 @@
+package hello.aopre.internal;
+
+import hello.aopre.internal.aop.CallLogAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Slf4j
+@Import(CallLogAspect.class)
+@SpringBootTest
+class CallServiceV1Test {
+
+    @Autowired
+    CallServiceV1 callService;
+    @Test
+    void external() {
+        //log.info("target={}",callServiceV0.getClass());
+        callService.external();
+    }
+
+    @Test
+    void internal() {
+        callService.internal();
+    }
+}
