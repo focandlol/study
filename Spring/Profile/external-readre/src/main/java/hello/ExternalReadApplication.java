8c910095d885a58420bacf3ea7ff7c1ab5daf2e1
focandlol <focandlol@naver.com>
2024-03-24 11:47:07 +0900

    Use external settings (@ConfigurationProperties)

--- Spring/Profile/external-readre/src/main/java/hello/ExternalReadApplication.java
+++ Spring/Profile/external-readre/src/main/java/hello/ExternalReadApplication.java
@@ -1,14 +1,18 @@
 package hello;
 
+import hello.config.MyDataSourceConfigV1;
 import hello.config.MyDataSourceEnvConfig;
 import hello.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(scanBasePackages = "hello.dataSource")
+@ConfigurationPropertiesScan
 //@Import(MyDataSourceEnvConfig.class)
-@Import(MyDataSourceValueConfig.class)
+//@Import(MyDataSourceValueConfig.class)
+@Import(MyDataSourceConfigV1.class)
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
