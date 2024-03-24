7ea29e23d37d14efb50e1299720a5f37038adfd5
focandlol <focandlol@naver.com>
2024-03-24 18:12:46 +0900

    yaml

--- Spring/Profile/external-readre/src/main/java/hello/ExternalReadApplication.java
+++ Spring/Profile/external-readre/src/main/java/hello/ExternalReadApplication.java
@@ -6,13 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Import;
 
-@SpringBootApplication(scanBasePackages = "hello.dataSource")
+@SpringBootApplication(scanBasePackages = {"hello.dataSource","hello.pay"})
 @ConfigurationPropertiesScan
 //@Import(MyDataSourceEnvConfig.class)
 //@Import(MyDataSourceValueConfig.class)
 //@Import(MyDataSourceConfigV1.class)
 //@Import(MyDataSourceConfigV2.class)
-@Import(MyDataSourceConfigV3.class)
+@Import({MyDataSourceConfigV3.class})
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
