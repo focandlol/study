e99207ce0ff72be3b0453b1b39181f82ee56b87c
focandlol <focandlol@naver.com>
2024-03-24 12:25:55 +0900

    @ConfigurationProperties valid

---
+++ Spring/Profile/external-readre/src/main/java/hello/config/MyDataSourceConfigV3.java
@@ -0,0 +1,24 @@
+package hello.config;
+
+import hello.dataSource.MyDataSource;
+import hello.dataSource.MyDataSourcePropertiesV2;
+import hello.dataSource.MyDataSourcePropertiesV3;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+
+@Slf4j
+//@EnableConfigurationProperties(MyDataSourcePropertiesV1.class)
+public class MyDataSourceConfigV3 {
+
+    private final MyDataSourcePropertiesV3 properties;
+
+
+    public MyDataSourceConfigV3(MyDataSourcePropertiesV3 properties) {
+        this.properties = properties;
+    }
+
+    @Bean
+    public MyDataSource myDataSource(){
+        return new MyDataSource(properties.getUrl(), properties.getUsername(), properties.getPassword(), properties.getEtc().getMaxConnection(),properties.getEtc().getTimeout(), properties.getEtc().getOptions());
+    }
+}
