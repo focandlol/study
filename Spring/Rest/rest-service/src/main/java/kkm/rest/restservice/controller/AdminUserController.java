adfa1a64064c50da4d71251e1a2ed9292b5a6d21
focandlol <focandlol@naver.com>
2024-03-27 12:08:57 +0900

    filtering each

---
+++ Spring/Rest/rest-service/src/main/java/kkm/rest/restservice/controller/AdminUserController.java
@@ -0,0 +1,51 @@
+package kkm.rest.restservice.controller;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import jakarta.validation.Valid;
+import kkm.rest.restservice.bean.AdminUser;
+import kkm.rest.restservice.bean.User;
+import kkm.rest.restservice.dao.UserDaoService;
+import kkm.rest.restservice.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin")
+public class AdminUserController {
+
+    private final UserDaoService service;
+
+    @GetMapping("/users/{id}")
+    public MappingJacksonValue retrieveUser4Admin(@PathVariable int id){
+        User user = service.findOne(id);
+
+        AdminUser adminUser = new AdminUser();
+        if(user == null){
+            throw new UserNotFoundException(String.format("ID[%s] not found",id));
+        }else{
+            BeanUtils.copyProperties(user,adminUser);
+        }
+
+        SimpleBeanPropertyFilter filter =
+                SimpleBeanPropertyFilter.filterOutAllExcept("id","name","joinDate","ssn");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo",filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(adminUser);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+
+}
--- Spring/Rest/rest-service/src/main/java/kkm/rest/restservice/dao/UserDaoService.java
+++ Spring/Rest/rest-service/src/main/java/kkm/rest/restservice/dao/UserDaoService.java
@@ -16,9 +16,9 @@ public class UserDaoService {
     private static int userCount = 3;
 
     static{
-        users.add(new User(1,"kkm1",new Date()));
-        users.add(new User(2,"kkm2",new Date()));
-        users.add(new User(3,"kkm3",new Date()));
+        users.add(new User(1,"kkm1",new Date(),"test1","111-111"));
+        users.add(new User(2,"kkm2",new Date(),"test2","222-222"));
+        users.add(new User(3,"kkm3",new Date(),"test3","333-333"));
     }
 
     public List<User> findAll(){
