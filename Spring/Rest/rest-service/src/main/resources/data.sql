d4ad48619d0b7ae2f64d87beb45a2c3997ec5547
focandlol <focandlol@naver.com>
2024-03-30 10:18:28 +0900

    Add init data, security config

--- Spring/Rest/rest-service/src/main/resources/data.sql
+++ Spring/Rest/rest-service/src/main/resources/data.sql
@@ -1,1 +1,3 @@
-insert into users(id, join_date,name,password,ssn) values(90001,now(),'User1','test111','707010-111111');
+insert into users(id, join_date,name,password,ssn) values(90001,now(),'User1','test111','707010-111112');
+insert into users(id, join_date,name,password,ssn) values(90002,now(),'User2','test222','707010-111113');
+insert into users(id, join_date,name,password,ssn) values(90003,now(),'User3','test333','707010-111114');
