63a3281206c78e3364054146a380d41fa79c8334
focandlol <focandlol@naver.com>
2024-04-01 15:50:13 +0900

    jpa test

--- Spring/Rest/resttest/src/main/java/resttest/resttest/User.java
+++ Spring/Rest/resttest/src/main/java/resttest/resttest/User.java
@@ -1,14 +1,21 @@
 package resttest.resttest;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@Entity
+@NoArgsConstructor
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
