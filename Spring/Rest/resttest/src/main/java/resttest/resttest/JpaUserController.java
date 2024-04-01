e64640a4fbc8055c6c7da24ab0d7a3fe5b2844f7
focandlol <focandlol@naver.com>
2024-04-01 15:50:13 +0900

    jpa test

---
+++ Spring/Rest/resttest/src/main/java/resttest/resttest/JpaUserController.java
@@ -0,0 +1,49 @@
+package resttest.resttest;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/jpa")
+public class JpaUserController {
+
+    private final UserRepository userRepository;
+
+    @PostConstruct
+    public void a(){
+        User user = new User();
+        user.setName("a");
+        userRepository.save(user);
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers(){
+
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable int id){
+        Optional<User> user = userRepository.findById(id);
+
+
+
+        System.out.println("user = " + user.get());
+         EntityModel<User> entityModel = EntityModel.of(user.get());
+         entityModel.add(linkTo(methodOn(this.getClass()).retrieveAllUsers()).withRel("all-users"));
+         return entityModel;
+
+
+
+    }
+}
---
+++ Spring/Rest/resttest/src/main/java/resttest/resttest/UserRepository.java
@@ -0,0 +1,6 @@
+package resttest.resttest;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+}
