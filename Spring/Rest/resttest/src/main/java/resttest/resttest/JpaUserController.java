362cb1a7516b7378dd0eaeedce3187ac3429b6b9
focandlol <focandlol@naver.com>
2024-04-03 09:23:05 +0900

    JpaUserController userResource test

--- Spring/Rest/resttest/src/main/java/resttest/resttest/JpaUserController.java
+++ Spring/Rest/resttest/src/main/java/resttest/resttest/JpaUserController.java
@@ -39,11 +39,13 @@ public class JpaUserController {
 
 
         System.out.println("user = " + user.get());
-         EntityModel<User> entityModel = EntityModel.of(user.get());
+         /*EntityModel<User> entityModel = EntityModel.of(user.get());
          entityModel.add(linkTo(methodOn(this.getClass()).retrieveAllUsers()).withRel("all-users"));
-         return ResponseEntity.ok(entityModel);
+         return ResponseEntity.ok(entityModel);*/
 
-
+        UserResource userResource = new UserResource(user.get());
+        userResource.add(linkTo(methodOn(this.getClass()).retrieveAllUsers()).withRel("all-users"));
+        return ResponseEntity.ok(userResource);
 
     }
 }
