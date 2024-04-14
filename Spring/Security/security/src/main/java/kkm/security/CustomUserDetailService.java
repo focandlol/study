20f338d69665574cd0a729a86c5ba38357ce894f
focandlol <focandlol@naver.com>
2024-04-14 14:25:29 +0900

    Create SecurityContextRepository branch

--- Spring/Security/security/src/main/java/kkm/security/CustomUserDetailService.java
+++ Spring/Security/security/src/main/java/kkm/security/CustomUserDetailService.java
@@ -1,19 +1,19 @@
-package kkm.security;
-
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-import java.util.List;
-
-public class CustomUserDetailService implements UserDetailsService {
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-
-        AccountDto accountDto = new AccountDto("user", "{noop}2222", List.of(new SimpleGrantedAuthority("ROLE_USER")));
-        CustomUserDetails customUserDetails = new CustomUserDetails(accountDto);
-
-        return customUserDetails;
-    }
-}
+//package kkm.security;
+//
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//
+//import java.util.List;
+//
+//public class CustomUserDetailService implements UserDetailsService {
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//
+//        AccountDto accountDto = new AccountDto("user", "{noop}2222", List.of(new SimpleGrantedAuthority("ROLE_USER")));
+//        CustomUserDetails customUserDetails = new CustomUserDetails(accountDto);
+//
+//        return customUserDetails;
+//    }
+//}
