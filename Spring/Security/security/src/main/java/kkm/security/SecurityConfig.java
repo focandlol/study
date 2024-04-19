a676cdbbf24bc5c913cee01cff6cbf0de4c1b68d
focandlol <focandlol@naver.com>
2024-04-19 12:31:19 +0900

    Config CorsConfigurationSource

--- Spring/Security/security/src/main/java/kkm/security/SecurityConfig.java
+++ Spring/Security/security/src/main/java/kkm/security/SecurityConfig.java
@@ -21,6 +21,9 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.io.IOException;
 
@@ -198,13 +201,13 @@ public class SecurityConfig {
         /**
          * exceptionHandling()
          */
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
-                        .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .exceptionHandling(exception -> exception
+//        http
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/login").permitAll()
+//                        .requestMatchers("/admin").hasRole("ADMIN")
+//                        .anyRequest().authenticated())
+//                .formLogin(Customizer.withDefaults())
+//                .exceptionHandling(exception -> exception
 //                        .authenticationEntryPoint(new AuthenticationEntryPoint() {
 //                            @Override
 //                            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
@@ -212,13 +215,37 @@ public class SecurityConfig {
 //                                response.sendRedirect("/login");
 //                            }
 //                        })
-                        .accessDeniedHandler(((request, response, accessDeniedException) -> {
-                            System.out.println("accessDeniedException.getMessage() = " + accessDeniedException.getMessage());
-                            response.sendRedirect("/denied");
-                        }))
-                )
+//                        .accessDeniedHandler(((request, response, accessDeniedException) -> {
+//                            System.out.println("accessDeniedException.getMessage() = " + accessDeniedException.getMessage());
+//                            response.sendRedirect("/denied");
+//                        }))
+//                )
+//                ;
+
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 ;
         return http.build();
+
+
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource(){
+
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:8080");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+        config.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",config);
+        return source;
     }
 
     @Bean
