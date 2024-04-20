d090f5eebb241fc4be542b594fecfbb0da965b64
focandlol <focandlol@naver.com>
2024-04-20 14:43:05 +0900

    Change project

---
+++ Spring/Security/sec/kkm/src/test/java/sec/kkm/KkmApplicationTests.java
@@ -0,0 +1,13 @@
+package sec.kkm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
