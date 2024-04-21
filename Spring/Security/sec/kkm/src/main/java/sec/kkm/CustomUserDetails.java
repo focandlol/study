feb9931d3b9ad08076d4e3642f65a7e67d4da4fa
focandlol <focandlol@naver.com>
2024-04-21 11:16:38 +0900

    Config csrfTokenRepository cookie

--- Spring/Security/sec/kkm/src/main/java/sec/kkm/CustomUserDetails.java
+++ Spring/Security/sec/kkm/src/main/java/sec/kkm/CustomUserDetails.java
@@ -1,69 +1,69 @@
 package sec.kkm;//package kkm.security;
-//
-//import lombok.EqualsAndHashCode;
-//import org.springframework.security.core.GrantedAuthority;
-//import org.springframework.security.core.userdetails.UserDetails;
-//
-//import java.util.Collection;
-//import java.util.Objects;
-//
-//public class CustomUserDetails implements UserDetails {
-//
-//    private final AccountDto accountDto;
-//
-//    public CustomUserDetails(AccountDto accountDto) {
-//        this.accountDto = accountDto;
-//    }
-//
-//    public AccountDto getAccountDto() {
-//        return accountDto;
-//    }
-//
-//    @Override
-//    public Collection<? extends GrantedAuthority> getAuthorities() {
-//        return accountDto.getAuthorities();
-//    }
-//
-//    @Override
-//    public String getPassword() {
-//        return accountDto.getPassword();
-//    }
-//
-//    @Override
-//    public String getUsername() {
-//        return accountDto.getUsername();
-//    }
-//
-//    @Override
-//    public boolean isAccountNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isAccountNonLocked() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isCredentialsNonExpired() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean isEnabled() {
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        CustomUserDetails that = (CustomUserDetails) o;
-//        return Objects.equals(getAccountDto(), that.getAccountDto());
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(getAccountDto());
-//    }
-//}
+
+import lombok.EqualsAndHashCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Objects;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final AccountDto accountDto;
+
+    public CustomUserDetails(AccountDto accountDto) {
+        this.accountDto = accountDto;
+    }
+
+    public AccountDto getAccountDto() {
+        return accountDto;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return accountDto.getAuthorities();
+    }
+
+    @Override
+    public String getPassword() {
+        return accountDto.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return accountDto.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CustomUserDetails that = (CustomUserDetails) o;
+        return Objects.equals(getAccountDto(), that.getAccountDto());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getAccountDto());
+    }
+}
