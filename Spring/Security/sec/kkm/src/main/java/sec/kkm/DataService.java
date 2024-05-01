9c25189b00bd019c4364a6877911f12340bfd7f8
focandlol <focandlol@naver.com>
2024-05-01 12:16:27 +0900

    Add readList api in MethodController and dataService

--- Spring/Security/sec/kkm/src/main/java/sec/kkm/DataService.java
+++ Spring/Security/sec/kkm/src/main/java/sec/kkm/DataService.java
@@ -1,9 +1,11 @@
 package sec.kkm;
 
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -18,5 +20,14 @@ public class DataService {
     @PreFilter("filterObject.value.owner == authentication.name")
     public Map<String,Account> writeMap(Map<String,Account> accounts) {
         return accounts;
+    }
+
+    @PostFilter("filterObject.owner == authentication.name")
+    public List<Account> readList() {
+        return new ArrayList<>(List.of(
+                new Account("user",false),
+                new Account("admin",false),
+                new Account("db",false)
+        ));
     }
 }
