544dc93318b0197835c63452c0300fb371ab0872
focandlol <focandlol@naver.com>
2024-05-01 11:09:24 +0900

    Add IndexController api

---
+++ Spring/Security/sec/kkm/src/main/java/sec/kkm/MethodController.java
@@ -0,0 +1,34 @@
+package sec.kkm;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MethodController {
+
+    @GetMapping("/adminMethod")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public String adminMethod() {
+        return "adminMethod";
+    }
+
+    @GetMapping("/userMethod")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN','ROLE_USER')")
+    public String userMethod() {
+        return "userMethod";
+    }
+
+    @GetMapping("/isAuthenticated")
+    @PreAuthorize("isAuthenticated()")
+    public String isuthenticated() {
+        return "isAuthenticated";
+    }
+
+    @GetMapping("/userMethod/{id}")
+    @PreAuthorize("#id == authentication.name")
+    public String authentication(@PathVariable String id) {
+        return id;
+    }
+}
--- Spring/Security/sec/kkm/src/main/java/sec/kkm/SecurityConfig.java
+++ Spring/Security/sec/kkm/src/main/java/sec/kkm/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -20,6 +21,7 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
 
 @EnableWebSecurity
+@EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
 
@@ -116,6 +118,11 @@ public class SecurityConfig {
         /**
          * securityMatcher
          */
+//        http
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .anyRequest().authenticated())
+//                .formLogin(Customizer.withDefaults());
+
         http
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated())
@@ -126,16 +133,16 @@ public class SecurityConfig {
     /**
      * securityMatcher filterChain
      */
-    @Bean
-    @Order(1)
-    public SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
-        http
-                .securityMatchers(matchers -> matchers.requestMatchers("/api/**","/oauth/**"))
-                .authorizeHttpRequests(authorize -> authorize
-                        .anyRequest().permitAll())
-                ;
-        return http.build();
-    }
+//    @Bean
+//    @Order(1)
+//    public SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
+//        http
+//                .securityMatchers(matchers -> matchers.requestMatchers("/api/**","/oauth/**"))
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .anyRequest().permitAll())
+//                ;
+//        return http.build();
+//    }
 
     @Bean
     public UserDetailsService userDetailsService(){
