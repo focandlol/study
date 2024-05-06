abeaeb8cab501af18fb8a9ada501ecfb98b0490b
focandlol <focandlol@naver.com>
2024-05-06 10:16:53 +0900

    Add customMethodInterceptor bean for customAdvisor in MethodSecurityConfig

--- Spring/Security/sec/kkm/src/main/java/sec/kkm/method/CustomMethodInterceptor.java
+++ Spring/Security/sec/kkm/src/main/java/sec/kkm/method/CustomMethodInterceptor.java
@@ -5,6 +5,7 @@ import org.aopalliance.intercept.MethodInvocation;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class CustomMethodInterceptor implements MethodInterceptor {
