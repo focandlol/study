8f4abb679f2a0a4f448fd14f775a7bdd0b9c53b3
focandlol <focandlol@naver.com>
2024-05-08 11:52:48 +0900

    Add CustomAuthenticationProvider2

--- Spring/Security/sec/kkm/src/main/java/sec/kkm/event/CustomAuthenticationProvider2.java
+++ Spring/Security/sec/kkm/src/main/java/sec/kkm/event/CustomAuthenticationProvider2.java
@@ -1,35 +1,30 @@
 package sec.kkm.event;
 
-import lombok.RequiredArgsConstructor;
-import org.apache.catalina.core.ApplicationContext;
-import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.event.AuthenticationFailureProviderNotFoundEvent;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Component;
 
-//@Component
-@RequiredArgsConstructor
-public class CustomAuthenticationProvider implements AuthenticationProvider {
+public class CustomAuthenticationProvider2 implements AuthenticationProvider {
 
-    private final ApplicationEventPublisher applicationEventPublisher;
+    private final AuthenticationEventPublisher authenticationEventPublisher;
+
+    public CustomAuthenticationProvider2(AuthenticationEventPublisher authenticationEventPublisher) {
+        this.authenticationEventPublisher = authenticationEventPublisher;
+    }
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
 
         if(!authentication.getName().equals("user")) {
-
-            applicationEventPublisher.publishEvent
-                   (new AuthenticationFailureProviderNotFoundEvent(authentication, new BadCredentialsException("BadCredentialException")));
+            authenticationEventPublisher.publishAuthenticationFailure(new BadCredentialsException("DisabledException"), authentication);
 
             throw new BadCredentialsException("BadCredentialsException");
         }
-
         UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
         return new UsernamePasswordAuthenticationToken(user, user.getPassword(), user.getAuthorities());
     }
