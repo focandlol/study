28a6258a6594d3563eee9a1d0b4737552955533a
focandlol <focandlol@naver.com>
2024-05-08 11:08:53 +0900

    Add CustomAuthenticationSuccessEvent

---
+++ Spring/Security/sec/kkm/src/main/java/sec/kkm/event/CustomAuthenticationSuccessEvent.java
@@ -0,0 +1,11 @@
+package sec.kkm.event;
+
+import org.springframework.security.authentication.event.AbstractAuthenticationEvent;
+import org.springframework.security.core.Authentication;
+
+public class CustomAuthenticationSuccessEvent extends AbstractAuthenticationEvent {
+
+    public CustomAuthenticationSuccessEvent(Authentication authentication) {
+        super(authentication);
+    }
+}
