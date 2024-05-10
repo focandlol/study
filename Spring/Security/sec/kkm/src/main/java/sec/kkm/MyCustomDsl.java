f74ef61d8d2e4b4e4def1607a6d3ce69db701e5c
focandlol <focandlol@naver.com>
2024-05-10 11:14:41 +0900

    Add custom dsl

---
+++ Spring/Security/sec/kkm/src/main/java/sec/kkm/MyCustomDsl.java
@@ -0,0 +1,31 @@
+package sec.kkm;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
+
+public class MyCustomDsl extends AbstractHttpConfigurer<MyCustomDsl, HttpSecurity> {
+
+
+    private boolean flag;
+
+    @Override
+    public void init(HttpSecurity http) throws Exception {
+        super.init(http);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        MyCustomFilter myCustomFilter = new MyCustomFilter();
+        myCustomFilter.setFlag(flag);
+        http.addFilterAfter(myCustomFilter, SecurityContextHolderAwareRequestFilter.class);
+    }
+
+    public MyCustomDsl flag(boolean value) {
+        this.flag = value;
+        return this;
+    }
+    public static MyCustomDsl customDsl() {
+        return new MyCustomDsl();
+    }
+}
