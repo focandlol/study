f01e443152c0aaaaca61352b249064bcc99d4f9e
focandlol <focandlol@naver.com>
2024-05-11 13:24:30 +0900

    Add custom formAuthenticationProvider

--- Spring/Security/securityProject/src/main/java/kkm/securityProject/security/config/SecurityConfig.java
+++ Spring/Security/securityProject/src/main/java/kkm/securityProject/security/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package kkm.securityProject.security.config;
 
+import kkm.securityProject.security.provider.FormAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -19,7 +21,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final UserDetailsService userDetailsService;
+    private final AuthenticationProvider authenticationProvider;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -29,13 +31,10 @@ public class SecurityConfig {
                         .anyRequest().authenticated())
 
                 .formLogin(form -> form.loginPage("/login").permitAll())
-                .userDetailsService(userDetailsService)
+                .authenticationProvider(authenticationProvider)
         ;
         return http.build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
+
 }
