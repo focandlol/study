c1bf14c6d814ff52cc4fb1a5bfe6308547ec7313
focandlol <focandlol@naver.com>
2024-05-12 13:19:11 +0900

    Add /denied api in LoginController

--- Spring/Security/securityProject/src/main/java/kkm/securityProject/users/controller/LoginController.java
+++ Spring/Security/securityProject/src/main/java/kkm/securityProject/users/controller/LoginController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kkm.securityProject.domain.dto.AccountDto;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
@@ -40,5 +41,12 @@ public class LoginController {
         return "redirect:/";
     }
 
+    @GetMapping("/denied")
+    public String accessDenied(@RequestParam(value = "exception",required = false) String exception,
+                               @AuthenticationPrincipal AccountDto accountDto, Model model){
+        model.addAttribute("username",accountDto.getUsername());
+        model.addAttribute("exception",exception);
 
+        return "login/denied";
+    }
 }
