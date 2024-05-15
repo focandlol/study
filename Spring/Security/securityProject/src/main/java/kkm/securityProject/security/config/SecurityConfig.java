1d4ea0a7b2c2b52af9ecedf452f98b909ee8cb78
focandlol <focandlol@naver.com>
2024-05-15 12:59:04 +0900

    Config csrf and Add RestApiDsl

--- Spring/Security/securityProject/src/main/java/kkm/securityProject/security/config/SecurityConfig.java
+++ Spring/Security/securityProject/src/main/java/kkm/securityProject/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package kkm.securityProject.security.config;
 
 import jakarta.servlet.http.HttpServletRequest;
 import kkm.securityProject.security.details.FormWebAuthenticationDetailsSource;
+import kkm.securityProject.security.dsl.RestApiDsl;
 import kkm.securityProject.security.entrypoint.RestAuthenticationEntryPoint;
 import kkm.securityProject.security.filters.RestAuthenticationFilter;
 import kkm.securityProject.security.handler.*;
@@ -82,24 +83,31 @@ public class SecurityConfig {
                         .requestMatchers("/api/manager").hasAuthority("ROLE_MANAGER")
                         .requestMatchers("/api/admin").hasAuthority("ROLE_ADMIN")
                         .anyRequest().permitAll())
-                .csrf(AbstractHttpConfigurer::disable)
+               // .csrf(AbstractHttpConfigurer::disable)
 
-                .addFilterBefore(restAuthenticationFilter(http,authenticationManager), UsernamePasswordAuthenticationFilter.class)
+                //.addFilterBefore(restAuthenticationFilter(http,authenticationManager), UsernamePasswordAuthenticationFilter.class)
                 .authenticationManager(authenticationManager)
                 .exceptionHandling(exception -> exception
                         .authenticationEntryPoint(new RestAuthenticationEntryPoint())
                         .accessDeniedHandler(new RestAccessDeniedHandler())
                 )
+                .with(new RestApiDsl<>(), restDsl -> restDsl
+                        .restSuccessHandler(restAuthenticationSuccessHandler)
+                        .restFailureHandler(restAuthenticationFailureHandler)
+                        .loginPage("/api/login")
+                        .loginProcessingUrl("/api/login")
+                )
+
         ;
         return http.build();
     }
 
-    private RestAuthenticationFilter restAuthenticationFilter(HttpSecurity http,AuthenticationManager authenticationManager) {
-        RestAuthenticationFilter restAuthenticationFilter = new RestAuthenticationFilter(http);
-        restAuthenticationFilter.setAuthenticationManager(authenticationManager);
-        restAuthenticationFilter.setAuthenticationSuccessHandler(restAuthenticationSuccessHandler);
-        restAuthenticationFilter.setAuthenticationFailureHandler(restAuthenticationFailureHandler);
-        return restAuthenticationFilter;
-    }
+//    private RestAuthenticationFilter restAuthenticationFilter(HttpSecurity http,AuthenticationManager authenticationManager) {
+//        RestAuthenticationFilter restAuthenticationFilter = new RestAuthenticationFilter(http);
+//        restAuthenticationFilter.setAuthenticationManager(authenticationManager);
+//        restAuthenticationFilter.setAuthenticationSuccessHandler(restAuthenticationSuccessHandler);
+//        restAuthenticationFilter.setAuthenticationFailureHandler(restAuthenticationFailureHandler);
+//        return restAuthenticationFilter;
+//    }
 
 }
