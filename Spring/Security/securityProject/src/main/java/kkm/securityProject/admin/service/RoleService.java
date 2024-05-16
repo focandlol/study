4f093409ccd4139118104d65621c8be2da397f0d
focandlol <focandlol@naver.com>
2024-05-16 11:20:36 +0900

    Add admin/service package

---
+++ Spring/Security/securityProject/src/main/java/kkm/securityProject/admin/service/RoleService.java
@@ -0,0 +1,12 @@
+package kkm.securityProject.admin.service;
+
+public interface RoleService {
+
+    Role getRole(long id);
+    List<Role> getRoles();
+    List<Role> getRolesWithoutExpression();
+
+    void createRole(Role role);
+
+    void deleteRole(long id);
+}
