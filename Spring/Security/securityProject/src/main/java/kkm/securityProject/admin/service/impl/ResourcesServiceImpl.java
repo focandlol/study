f177338570861d611938df5f80c8af47ca23e452
focandlol <focandlol@naver.com>
2024-05-16 11:22:43 +0900

    Add admin/service/impl package

---
+++ Spring/Security/securityProject/src/main/java/kkm/securityProject/admin/service/impl/ResourcesServiceImpl.java
@@ -0,0 +1,37 @@
+package kkm.securityProject.admin.service.impl;
+
+import kkm.securityProject.admin.repository.ResourcesRepository;
+import kkm.securityProject.admin.service.ResourcesService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ResourcesServiceImpl implements ResourcesService {
+
+    private final ResourcesRepository resourcesRepository;
+
+    @Transactional
+    public Resources getResources(long id) {
+        return resourcesRepository.findById(id).orElse(new Resources());
+    }
+
+    @Transactional
+    public List<Resources> getResources() {
+        return resourcesRepository.findAll(Sort.by(Sort.Order.asc("orderNum")));
+    }
+
+    @Transactional
+    public void createResources(Resources resources){
+        resourcesRepository.save(resources);
+    }
+
+    @Transactional
+    public void deleteResources(long id) {
+        resourcesRepository.deleteById(id);
+    }
+}
