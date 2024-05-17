aa95a77dcda2fcfa427bf237bb0569151f5ae363
focandlol <focandlol@naver.com>
2024-05-17 12:39:39 +0900

    Add reload method in CustomDynamicAuthorizationManager

--- Spring/Security/securityProject/src/main/java/kkm/securityProject/admin/service/impl/ResourcesServiceImpl.java
+++ Spring/Security/securityProject/src/main/java/kkm/securityProject/admin/service/impl/ResourcesServiceImpl.java
@@ -3,9 +3,11 @@ package kkm.securityProject.admin.service.impl;
 import kkm.securityProject.admin.repository.ResourcesRepository;
 import kkm.securityProject.admin.service.ResourcesService;
 import kkm.securityProject.domain.entity.Resources;
+import kkm.securityProject.security.manager.CustomDynamicAuthorizationManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +19,7 @@ import java.util.List;
 public class ResourcesServiceImpl implements ResourcesService {
 
     private final ResourcesRepository resourcesRepository;
+    private CustomDynamicAuthorizationManager authorizationManager;
 
     @Transactional
     public Resources getResources(long id) {
@@ -31,10 +34,12 @@ public class ResourcesServiceImpl implements ResourcesService {
     @Transactional
     public void createResources(Resources resources){
         resourcesRepository.save(resources);
+        authorizationManager.reload();
     }
 
     @Transactional
     public void deleteResources(long id) {
         resourcesRepository.deleteById(id);
+        authorizationManager.reload();
     }
 }
