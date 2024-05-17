72bfe75e824a345cd9e455b518d818a07ae3acf2
focandlol <focandlol@naver.com>
2024-05-17 12:20:18 +0900

    Add PersistenceUrlRoleMapper db

--- Spring/Security/securityProject/src/main/java/kkm/securityProject/security/mapper/PersistentUrlRoleMapper.java
+++ Spring/Security/securityProject/src/main/java/kkm/securityProject/security/mapper/PersistentUrlRoleMapper.java
@@ -1,10 +1,29 @@
 package kkm.securityProject.security.mapper;
 
+import kkm.securityProject.admin.repository.ResourcesRepository;
+import kkm.securityProject.domain.entity.Resources;
+
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
-public class PersistentUrlRoleMapper implements UrlRoleMapper {
+public class PersistentUrlRoleMapper implements UrlRoleMapper{
+
+    private final LinkedHashMap<String, String> urlRoleMappings = new LinkedHashMap<>();
+    private final ResourcesRepository resourcesRepository;
+
+    public PersistentUrlRoleMapper(ResourcesRepository resourcesRepository) {
+        this.resourcesRepository = resourcesRepository;
+    }
     @Override
     public Map<String, String> getUrlRoleMappings() {
-        return Map.of();
+
+        List<Resources> resourcesList = resourcesRepository.findAllResources();
+        resourcesList.forEach(re -> {
+            re.getRoleSet().forEach(role -> {
+                urlRoleMappings.put(re.getResourceName(), role.getRoleName());
+            });
+        });
+        return urlRoleMappings;
     }
 }
