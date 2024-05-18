b77ea80b7c75e39df75b70f38cb7507618d3792c
focandlol <focandlol@naver.com>
2024-05-18 19:35:08 +0900

    Add RequestMatcherDynamicAuthorizationManager

---
+++ Spring/Security/securityProject/src/main/java/kkm/securityProject/security/manager/RequestMatcherDynamicAuthorizationManager.java
@@ -0,0 +1,88 @@
+package kkm.securityProject.security.manager;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import kkm.securityProject.admin.repository.ResourcesRepository;
+import kkm.securityProject.security.mapper.PersistentUrlRoleMapper;
+import kkm.securityProject.security.service.DynamicAuthorizationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.access.expression.DefaultHttpSecurityExpressionHandler;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcherEntry;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class RequestMatcherDynamicAuthorizationManager implements AuthorizationManager<HttpServletRequest> {
+    List<RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>>> mappings;
+    //    private static final AuthorizationDecision DENY = new AuthorizationDecision(false);
+    private static final AuthorizationDecision ACCESS = new AuthorizationDecision(true);
+    private final HandlerMappingIntrospector handlerMappingIntrospector;
+    private final ResourcesRepository resourcesRepository;
+    private final RoleHierarchy roleHierarchy;
+
+    @PostConstruct
+    public void mapping() {
+
+        DynamicAuthorizationService dynamicAuthorizationService =
+                new DynamicAuthorizationService(new PersistentUrlRoleMapper(resourcesRepository));
+
+        mappings = dynamicAuthorizationService.getUrlRoleMappings()
+                .entrySet().stream()
+                .map(entry -> new RequestMatcherEntry<>(
+                        new MvcRequestMatcher(handlerMappingIntrospector, entry.getKey()),
+                        customAuthorizationManager(entry.getValue())))
+                .collect(Collectors.toList());
+    }
+    @Override
+    public AuthorizationDecision check(Supplier<Authentication> authentication, HttpServletRequest request) {
+
+        for (RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> mapping : this.mappings) {
+
+            RequestMatcher matcher = mapping.getRequestMatcher();
+            RequestMatcher.MatchResult matchResult = matcher.matcher(request);
+
+            if (matchResult.isMatch()) {
+                AuthorizationManager<RequestAuthorizationContext> manager = mapping.getEntry();
+                return manager.check(authentication,
+                        new RequestAuthorizationContext(request, matchResult.getVariables()));
+            }
+        }
+        return ACCESS;
+    }
+
+    @Override
+    public void verify(Supplier<Authentication> authentication, HttpServletRequest request) {
+        AuthorizationManager.super.verify(authentication, request);
+    }
+
+    private AuthorizationManager<RequestAuthorizationContext> customAuthorizationManager(String role) {
+        if (role.startsWith("ROLE")) {
+            AuthorityAuthorizationManager<RequestAuthorizationContext> authorizationManager
+                    = AuthorityAuthorizationManager.hasAuthority(role);
+            authorizationManager.setRoleHierarchy(roleHierarchy);
+            return authorizationManager;
+        }else{
+            DefaultHttpSecurityExpressionHandler handler = new DefaultHttpSecurityExpressionHandler();
+            handler.setRoleHierarchy(roleHierarchy);
+            WebExpressionAuthorizationManager authorizationManager
+                    = new WebExpressionAuthorizationManager(role);
+            authorizationManager.setExpressionHandler(handler);
+
+            return authorizationManager;
+        }
+    }
+}
