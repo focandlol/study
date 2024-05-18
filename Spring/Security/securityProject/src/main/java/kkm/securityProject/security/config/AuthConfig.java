f6495214449aca61e721124b0ae4adf5e27635ea
focandlol <focandlol@naver.com>
2024-05-18 10:41:56 +0900

    Add RoleHierarchyImpl bean in AuthConfig and add roleHierarchy authorizationManagers in CustomDynamicAuthorizationManager

--- Spring/Security/securityProject/src/main/java/kkm/securityProject/security/config/AuthConfig.java
+++ Spring/Security/securityProject/src/main/java/kkm/securityProject/security/config/AuthConfig.java
@@ -1,7 +1,10 @@
 package kkm.securityProject.security.config;
 
+import kkm.securityProject.admin.service.RoleHierarchyService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -11,5 +14,14 @@ public class AuthConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public RoleHierarchy roleHierarchy(RoleHierarchyService roleHierarchyService) {
+        String allHierarchy = roleHierarchyService.findAllHierarchy();
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy(allHierarchy);
+
+        return roleHierarchy;
     }
 }
