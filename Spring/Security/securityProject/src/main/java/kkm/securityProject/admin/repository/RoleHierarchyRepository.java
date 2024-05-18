73e0730480022ee1d2683a45f904e727f76d6bfe
focandlol <focandlol@naver.com>
2024-05-18 10:26:08 +0900

    Config RoleHierarchy

---
+++ Spring/Security/securityProject/src/main/java/kkm/securityProject/admin/repository/RoleHierarchyRepository.java
@@ -0,0 +1,8 @@
+package kkm.securityProject.admin.repository;
+
+import kkm.securityProject.domain.entity.RoleHierarchy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface RoleHierarchyRepository extends JpaRepository<RoleHierarchy, Long> {
+}
---
+++ Spring/Security/securityProject/src/main/java/kkm/securityProject/admin/service/RoleHierarchyService.java
@@ -0,0 +1,5 @@
+package kkm.securityProject.admin.service;
+
+public interface RoleHierarchyService {
+    String findAllHierarchy();
+}
