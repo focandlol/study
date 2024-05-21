004eedba39594cfebe5dda9a26612655bcece1b5
focandlol <focandlol@naver.com>
2024-05-21 11:40:48 +0900

    Add ObjectBox > wrong type arg problem

---
+++ Java/java-mid2/src/ganeric/ex1/BoxMain2.java
@@ -0,0 +1,20 @@
+package ganeric.ex1;
+
+public class BoxMain2 {
+
+    public static void main(String[] args) {
+        ObjectBox integerBox = new ObjectBox();
+        integerBox.set(10);
+        Integer integer = (Integer) integerBox.get();
+        System.out.println("integer = " + integer);
+
+        ObjectBox stringBox = new ObjectBox();
+        stringBox.set("hello");
+        String str = (String) stringBox.get();
+        System.out.println("str = " + str);
+
+       // integerBox.set("wrong"); //wrong type arg
+        //Integer i = (Integer) integerBox.get();
+        //System.out.println("i = " + i);
+    }
+}
