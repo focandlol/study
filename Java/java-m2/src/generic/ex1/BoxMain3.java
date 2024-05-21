3f0dac6a16b5339d47d8c1c43de6a8ff9b294ad7
focandlol <focandlol@naver.com>
2024-05-21 12:26:29 +0900

    Add GenericBox > solve befor problem

---
+++ Java/java-m2/src/generic/ex1/BoxMain3.java
@@ -0,0 +1,16 @@
+package generic.ex1;
+
+public class BoxMain3 {
+
+    public static void main(String[] args) {
+        GenericBox<Integer> integerBox = new GenericBox<Integer>();
+        integerBox.set(10);
+        Integer i = integerBox.get();
+        System.out.println("i = " + i);
+
+        GenericBox<String> stringBox = new GenericBox<>();
+        stringBox.set("hello");
+        String s = stringBox.get();
+        System.out.println("s = " + s);
+    }
+}
