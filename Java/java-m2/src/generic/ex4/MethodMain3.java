e7c79e685eb081ce1b5f4a7cc20c8ad6e68acb8d
focandlol <focandlol@naver.com>
2024-05-22 12:28:45 +0900

    Add AnimalMethod, ComplexBox > utilize generic mehtod

---
+++ Java/java-m2/src/generic/ex4/MethodMain3.java
@@ -0,0 +1,20 @@
+package generic.ex4;
+
+import generic.animal.Animal;
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class MethodMain3 {
+
+    public static void main(String[] args) {
+
+        Dog dog = new Dog("dog1", 100);
+        Cat cat = new Cat("cat1", 200);
+
+        ComplexBox<Dog> hospital = new ComplexBox<>();
+        hospital.set(dog);
+
+        Cat returnCat = hospital.printAndReturn(cat);
+        System.out.println("returnCat = " + returnCat);
+    }
+}
