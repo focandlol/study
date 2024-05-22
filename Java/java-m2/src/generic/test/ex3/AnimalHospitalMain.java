cb8d05e5120f58da614e4b2f3ed20d50f870fa3a
focandlol <focandlol@naver.com>
2024-05-22 11:18:01 +0900

    Add Cat,DogHospital > can't replace code problem

---
+++ Java/java-m2/src/generic/test/ex3/AnimalHospitalMain.java
@@ -0,0 +1,25 @@
+package generic.test.ex3;
+
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class AnimalHospitalMain {
+
+    public static void main(String[] args) {
+        DogHospital dogHospital = new DogHospital();
+        CatHospital catHospital = new CatHospital();
+
+        Dog dog = new Dog("멍1", 100);
+        Cat cat = new Cat("냥1", 300);
+
+        dogHospital.set(dog);
+        dogHospital.checkUp();
+
+        catHospital.set(cat);
+        catHospital.checkUp();
+
+        Dog biggerDog = dogHospital.bigger(new Dog("멍2", 300));
+        System.out.println("biggerDog = " + biggerDog);
+
+    }
+}
