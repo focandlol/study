03ffc7798817a36150a328fce6c740d6c45bab1b
focandlol <focandlol@naver.com>
2024-05-22 11:24:10 +0900

    Add AnimalHospitalV1 > low type safety problem

---
+++ Java/java-m2/src/generic/test/ex3/AnimalHospitalMainV1.java
@@ -0,0 +1,28 @@
+package generic.test.ex3;
+
+import generic.animal.Animal;
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class AnimalHospitalMainV1 {
+
+    public static void main(String[] args) {
+        AnimalHospitalV1 dogHospital = new AnimalHospitalV1();
+        AnimalHospitalV1 catHospital = new AnimalHospitalV1();
+
+        Dog dog = new Dog("멍1", 100);
+        Cat cat = new Cat("냥1", 300);
+
+        dogHospital.set(dog);
+        dogHospital.checkUp();
+
+        catHospital.set(cat);
+        catHospital.checkUp();
+
+        //dogHospital.set(cat); //매개변수 체크 실패 : 컴파일 오류가 발생하지 않음
+
+        Dog biggerDog = (Dog)dogHospital.bigger(new Dog("멍2", 300));
+        System.out.println("biggerDog = " + biggerDog);
+
+    }
+}
