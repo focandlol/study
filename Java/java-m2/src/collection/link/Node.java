f5548be42a5f6609795234b500100609c1001c38
focandlol <focandlol@naver.com>
2024-05-24 11:32:15 +0900

    Add toString() in Node

--- Java/java-m2/src/collection/link/Node.java
+++ Java/java-m2/src/collection/link/Node.java
@@ -8,4 +8,29 @@ public class Node {
     public Node(Object item){
         this.item = item;
     }
+
+    //idea가 자동으로 만들어준 toString()
+//    @Override
+//    public String toString() {
+//        return "Node{" +
+//                "item=" + item +
+//                ", next=" + next +
+//                '}';
+//    }
+
+    //직접 만든 toString()
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        Node x = this;
+        sb.append("[");
+        while (x != null){
+            sb.append(x.item);
+            if (x.next != null) {
+                sb.append("->");
+            }
+            x = x.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
