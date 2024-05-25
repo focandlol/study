af3a746f903b088f6aa3611a4987307c0c4cef3f
focandlol <focandlol@naver.com>
2024-05-25 12:54:36 +0900

    problem1 > ListEx1(Array to List), ListEx2(input output of list), ListEx3(total and sum)

---
+++ Java/java-m2/src/generic/test/ex3/ListEx1.java
@@ -0,0 +1,24 @@
+package generic.test.ex3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListEx1 {
+
+    public static void main(String[] args) {
+        List<Integer> studentList = new ArrayList<Integer>();
+        studentList.add(90);
+        studentList.add(80);
+        studentList.add(70);
+        studentList.add(60);
+        studentList.add(50);
+
+        int total = 0;
+        for (int i = 0; i < studentList.size(); i++) {
+            total += studentList.get(i);
+        }
+        double average = (double) total / studentList.size();
+        System.out.println("점수 총합: " + total);
+        System.out.println("점수 평균: " + average);
+    }
+}
