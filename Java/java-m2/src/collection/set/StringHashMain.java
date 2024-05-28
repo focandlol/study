9316b7c5fc639a337809d55c23f4b7f827aa0d01
focandlol <focandlol@naver.com>
2024-05-28 11:11:37 +0900

    Add myHashSetV1

---
+++ Java/java-m2/src/collection/set/StringHashMain.java
@@ -0,0 +1,14 @@
+package collection.set;
+
+public class StringHashMain {
+
+    static final int CAPACITY = 10;
+
+    public static void main(String[] args) {
+
+        char charA = 'A';
+        char charB = 'B';
+        System.out.println("charA = " + (int)charA);
+        System.out.println("charB = " + (int)charB);
+    }
+}
