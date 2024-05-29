cbf06aa030a63d3bb8a67cb6a3ac9e16537e92a6
focandlol <focandlol@naver.com>
2024-05-29 11:45:22 +0900

    problem3 remove duplicate and maintain output order > uniqueNamesTest3 using TreeSet()

---
+++ Java/java-m2/src/generic/test/ex4/UniqueNamesTest3.java
@@ -0,0 +1,23 @@
+package generic.test.ex4;
+
+import java.util.*;
+
+public class UniqueNamesTest3 {
+
+    public static void main(String[] args) {
+
+        Integer[] inputArr = {30,20,20,10,10};
+        List<Integer> inputArr1 = List.of(inputArr);
+
+        Set<Integer> set = new TreeSet<>(inputArr1);
+
+        /*for(Integer i : inputArr) {
+            set.add(i);
+        }*/
+
+        Iterator<Integer> iterator = set.iterator();
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
