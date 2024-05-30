3d72a010576116b65e608797791e137a3e0c55e6
focandlol <focandlol@naver.com>
2024-05-30 12:56:13 +0900

    map problem1 array to map > ArrayToMapTest

---
+++ Java/java-m2/src/generic/test/ex5/map/ArrayToMapTest.java
@@ -0,0 +1,23 @@
+package generic.test.ex5.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArrayToMapTest {
+
+    public static void main(String[] args) {
+        String[][] productArr = {{"Java", "10000"}, {"Spring", "20000"}, {"JPA",
+                "30000"}};
+
+        // 주어진 배열로부터 Map 생성 - 코드 작성
+        Map<String,Integer> map = new HashMap<>();
+        for (String[] product : productArr) {
+            map.put(product[0], Integer.parseInt(product[1]));
+        }
+        // Map의 모든 데이터 출력 - 코드 작성
+        for (String s : map.keySet()) {
+            System.out.println("제품:" + s + ", 가격: " + map.get(s));
+        }
+    }
+
+}
