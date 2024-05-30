12ea450bbf4315d30288560609c8d2760f91cd33
focandlol <focandlol@naver.com>
2024-05-30 11:28:21 +0900

    Add MapMain1

---
+++ Java/java-m2/src/collection/map/MapMain2.java
@@ -0,0 +1,15 @@
+package collection.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapMain2 {
+
+    public static void main(String[] args) {
+
+        Map<String, Integer> studentMap = new HashMap<>();
+
+        studentMap.put("studentA",90);
+
+    }
+}
