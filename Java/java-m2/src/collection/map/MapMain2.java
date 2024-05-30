f5c842f9f33dbc948e7d0f2c4456af149da4a401
focandlol <focandlol@naver.com>
2024-05-30 11:30:55 +0900

    Add MapMain2

--- Java/java-m2/src/collection/map/MapMain2.java
+++ Java/java-m2/src/collection/map/MapMain2.java
@@ -10,6 +10,15 @@ public class MapMain2 {
         Map<String, Integer> studentMap = new HashMap<>();
 
         studentMap.put("studentA",90);
+        System.out.println(studentMap);
 
+        studentMap.put("studentA",100); //같은 키에 저장시 기존 값 교체
+        System.out.println(studentMap);
+
+        boolean containsKey = studentMap.containsKey("studentA");
+        System.out.println(containsKey);
+
+        studentMap.remove("studentA");
+        System.out.println(studentMap);
     }
 }
