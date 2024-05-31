a97d3670006ea26918080eafe22ae86388cc1527
focandlol <focandlol@naver.com>
2024-05-31 15:13:35 +0900

    Add SortMain1,2 > Comparator desc,asc

--- Java/java-m2/src/collection/set/HashStart5.java
+++ Java/java-m2/src/collection/set/HashStart5.java
@@ -8,10 +8,14 @@ public class HashStart5 {
     static final int CAPACITY = 10;
 
     public static void main(String[] args) {
+        Object[] a = new Long[1];
         LinkedList<Integer>[] buckets = new LinkedList[CAPACITY];
+
+        System.out.println("buckets = " + Arrays.toString(buckets));
         for (int i = 0; i < CAPACITY; i++) {
             buckets[i] = new LinkedList<>();
         }
+
         System.out.println("buckets = " + Arrays.toString(buckets));
 
         add(buckets,1);
@@ -21,6 +25,7 @@ public class HashStart5 {
         add(buckets,14);
         add(buckets,99);
         add(buckets,9);
+
         System.out.println("buckets = " + Arrays.toString(buckets));
 
         int searchValue = 9;
