47050e035a1ea813c7cdb331e8cf90cfaecff8f2
focandlol <focandlol@naver.com>
2024-06-01 11:42:00 +0900

    Add OfMain > create immutable Collections using of() method

---
+++ Java/java-m2/src/collection/utils/OfMain.java
@@ -0,0 +1,21 @@
+package collection.utils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class OfMain {
+    public static void main(String[] args) {
+
+        //불변 컬렉션 생성, 값 변경,추가 불가
+        //변경 메서드 호출 시 UnsupportedOperationException 터짐
+        List<Integer> list = List.of(1, 2, 3);
+        Set<Integer> set = Set.of(1, 2, 3);
+        Map<Integer, String> map = Map.of(1, "one", 2, "two");
+
+        System.out.println("list = " + list);
+        System.out.println("set = " + set);
+        System.out.println("map = " + map);
+        System.out.println("list.getClass() = " + list.getClass());
+    }
+}
