78d2496d2254ca6bed8eeb44ed6e21f05003fc32
focandlol <focandlol@naver.com>
2024-06-06 12:55:30 +0900

    BaekJoon1874

--- CodingTest/DataStructure/src/IntervalSum/BaekJoon12891.java
+++ CodingTest/DataStructure/src/IntervalSum/BaekJoon12891.java
@@ -4,8 +4,6 @@ package IntervalSum;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class BaekJoon12891 {
@@ -14,7 +12,6 @@ public class BaekJoon12891 {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        List<String> compare = new ArrayList<>();
         long length = Long.parseLong(st.nextToken());
         long pwLength = Long.parseLong(st.nextToken());
         int[] aaa = new int[4];
@@ -27,12 +24,13 @@ public class BaekJoon12891 {
 
 
         int start = 0;
-        int end = 0;
+        int end = (int) (pwLength-1);
         while(end < length){
             if(end - start + 1 < pwLength){
                 end += 1;
             }
             else if(end - start + 1 == pwLength){
+                //System.out.println("hello");
                 int aCount = 0;
                 int cCount = 0;
                 int gCount = 0;
@@ -48,27 +46,17 @@ public class BaekJoon12891 {
                     }else{
                         tCount+=1;
                     }
-                    com = com.concat(split[i]);
                 }
                 if(aCount >= aaa[0] && cCount >= aaa[1] && gCount >= aaa[2] && tCount >= aaa[3]){
-                    boolean wi = false;
-                    for(int i=0; i<compare.size(); i++){
-                        if(compare.get(i).equals(com)){
-                            wi = true;
-                            break;
-                        }
-                    }
-                    if(wi == false){
-                        count+=1;
-                        compare.add(com);
-                    }
+
+                    count+=1;
                 }
-                start = end;
+                start +=1;
+                end+=1;
+
             }
             else if(end - start + 1 > pwLength){
-                for(int i=0; i<compare.size(); i++){
-                    if(compare.get(i).equals(split)){}
-                }
+
                 start += 1;
             }
         }
