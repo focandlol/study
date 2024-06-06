5b51f714d28ae229552bb6da4696538d16c9a261
focandlol <focandlol@naver.com>
2024-06-06 14:34:13 +0900

    BaekJoon11286

---
+++ CodingTest/DataStructure/src/stack/BaekJoon11286.java
@@ -0,0 +1,36 @@
+package stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+import static java.lang.Math.abs;
+
+public class BaekJoon11286 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> {
+            if(abs(a) == abs(b)){
+                return a > b ? 1 : -1;
+            }
+            return abs(a) - abs(b);
+        });
+
+        for(int i=0; i<num; i++){
+            int input = Integer.parseInt(br.readLine());
+
+            if(input == 0) {
+                if (pq.isEmpty()) {
+                    System.out.println("0");
+                }else{
+                    System.out.println(pq.poll());
+                }
+            }else{
+                pq.add(input);
+            }
+        }
+    }
+}
