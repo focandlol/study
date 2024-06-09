319a3b6bde83a75fa64a4ce4d4e5b0fe56d60348
focandlol <focandlol@naver.com>
2024-06-09 18:15:51 +0900

    BaekJoon2750 Quick sort (left)

---
+++ CodingTest/Sorting/src/quick/BaekJoon2750Left.java
@@ -0,0 +1,56 @@
+package quick;
+
+import java.util.Scanner;
+
+public class BaekJoon2750Left {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int[] arr = new int[num];
+        for(int i=0; i<num; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        quick(arr,0,num-1);
+        for(int i=0; i<num; i++){
+            System.out.println(arr[i]);
+        }
+    }
+
+    private static void quick(int[] arr, int i, int j){
+
+        if(i>=j){
+            return;
+        }
+        int pivot = partition(arr, i, j);
+
+        quick(arr,i,pivot-1);
+        quick(arr,pivot+1,j);
+
+    }
+
+    private static int partition(int[] arr, int i, int j){
+        int start = i;
+        int end = j;
+        int pivot = arr[i];
+
+        while(start < end){
+            while(arr[end] > pivot && start < end){
+                end--;
+            }
+            while(arr[start] <= pivot && start < end){
+                start++;
+            }
+            swap(arr,start,end);
+        }
+        swap(arr,start,i);
+        return start;
+    }
+
+    private static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i]  = arr[j];
+        arr[j] = temp;
+    }
+}
