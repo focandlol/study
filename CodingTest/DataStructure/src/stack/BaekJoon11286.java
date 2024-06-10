17c5fc479d785943b4172d0b1be7d476c004d3d0
focandlol <focandlol@naver.com>
2024-06-10 16:37:30 +0900

    BaekJoon2750 quick sort(left) update : Scanner -> BudderedReader

--- CodingTest/DataStructure/src/stack/BaekJoon11286.java
+++ CodingTest/DataStructure/src/stack/BaekJoon11286.java
@@ -18,19 +18,25 @@ public class BaekJoon11286 {
             }
             return abs(a) - abs(b);
         });
-
+        Queue<Integer> a = new LinkedList<>();
         for(int i=0; i<num; i++){
             int input = Integer.parseInt(br.readLine());
 
-            if(input == 0) {
-                if (pq.isEmpty()) {
-                    System.out.println("0");
-                }else{
-                    System.out.println(pq.poll());
-                }
-            }else{
-                pq.add(input);
-            }
+//            if(input == 0) {
+//                if (pq.isEmpty()) {
+//                    System.out.println("0");
+//                }else{
+//                    System.out.println(pq.poll());
+//                }
+//            }else{
+//                pq.add(input);
+//            }
+            pq.add(input);
+            a.add(input);
         }
+        System.out.println(a);
+        System.out.println(pq);
+        pq.poll();
+        System.out.println(pq);
     }
 }
