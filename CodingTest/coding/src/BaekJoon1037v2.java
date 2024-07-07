dd2498408a74a630ace98d936c11b0fa97f47e24
focandlol <focandlol@naver.com>
2024-07-07 17:56:18 +0900

    BaekJoon1037v2 real factors using sort

---
+++ CodingTest/coding/src/BaekJoon1037v2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BaekJoon1037v2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        if(n == 1){
+            System.out.println((int)Math.pow(arr[0],2));
+        }else{
+            System.out.println(arr[0]*arr[n-1]);
+        }
+
+    }
+}
