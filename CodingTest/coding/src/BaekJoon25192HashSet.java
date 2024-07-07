32006e7b9b7249e0f1d269ed7ca3614dab439d3e
focandlol <focandlol@naver.com>
2024-07-08 08:07:37 +0900

    BaekJoon25192 bright greeting gomgom using hashSet

---
+++ CodingTest/coding/src/BaekJoon25192HashSet.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class BaekJoon25192HashSet {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        HashSet<String> set = new HashSet<>();
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            String a = br.readLine();
+            if(a.equals("ENTER")){
+                count += set.size();
+                set.clear();
+            }else{
+                set.add(a);
+            }
+        }
+        count += set.size();
+        System.out.println(count);
+    }
+}
