03949b3281a8885714d2475213bb68ee9b0fbeab
focandlol <focandlol@naver.com>
2024-07-18 12:02:21 +0900

    BaekJoon24266 Algorithm execution time 5

---
+++ CodingTest/coding/src/complexity/BaekJoon24266.java
@@ -0,0 +1,13 @@
+package complexity;
+
+import java.util.Scanner;
+
+public class BaekJoon24266 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextInt();
+
+        System.out.println(n*n*n);
+        System.out.println("3");
+    }
+}
