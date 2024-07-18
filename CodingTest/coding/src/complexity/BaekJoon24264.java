0977afaba4e77cb75960261091ba4027a30edc34
focandlol <focandlol@naver.com>
2024-07-18 11:20:42 +0900

    BaekJoon24264 Algorithm execution time 3

---
+++ CodingTest/coding/src/complexity/BaekJoon24264.java
@@ -0,0 +1,16 @@
+package complexity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon24264 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.println((long)Math.pow(n, 2));
+        System.out.println("2");
+    }
+}
