4f1601d229dcd19d426c18724547515e0248021f
focandlol <focandlol@naver.com>
2024-07-19 11:06:25 +0900

    BaekJoon25501 master of recursion palindrome

---
+++ CodingTest/coding/src/recursion/BaekJoon25501.java
@@ -0,0 +1,30 @@
+package recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon25501 {
+    static int count;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<t; i++){
+            count = 0;
+            String a = br.readLine();
+            sb.append(isPalindrome(a)).append(" ").append(count).append("\n");
+        }
+        System.out.println(sb);
+    }
+    public static int recursion(String s, int l, int r){
+        count++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+    public static int isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
+    }
+}
