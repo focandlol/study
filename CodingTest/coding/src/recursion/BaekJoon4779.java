880366cce43363b55ce8ced400ea084c9bc603fc
focandlol <focandlol@naver.com>
2024-07-20 11:43:17 +0900

    BaekJoon4779 Cantorian set

---
+++ CodingTest/coding/src/recursion/BaekJoon4779.java
@@ -0,0 +1,34 @@
+package recursion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon4779 {
+    static StringBuilder sb;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str;
+        while(((str = br.readLine()) != null)){
+            sb = new StringBuilder();
+            int n = Integer.parseInt(str);
+            int cnt = (int) Math.pow(3,n);
+            find(cnt);
+            System.out.println(sb);
+        }
+    }
+
+    private static void find(int a) {
+        if(a == 1){
+            sb.append("-");
+            return;
+        }
+
+        find(a/3);
+        for(int i=0; i<a/3; i++){
+            sb.append(" ");
+        }
+        find(a/3);
+    }
+}
