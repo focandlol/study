17250c9bfbc6d1e4c581b775d816dc338dc185b1
focandlol <focandlol@naver.com>
2024-08-09 12:48:44 +0900

    BaekJoon17298_kkm

---
+++ CodingTest/coding/src/stack2/BaekJoon17298_kkm.java
@@ -0,0 +1,42 @@
+package stack2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BaekJoon17298_kkm {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        Stack<Integer> stack = new Stack<Integer>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 0; i < n; i++){
+
+            while(stack.size() > 0 && arr[stack.peek()] < arr[i]) {
+                arr[stack.pop()] = arr[i];
+            }
+
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()){
+            arr[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++){
+            sb.append(arr[i]).append(" ");
+        }
+        System.out.println(sb);
+
+    }
+}
