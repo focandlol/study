e98730b77f764579cab82065e7db1e216a520cce
focandlol <focandlol@naver.com>
2024-08-20 11:56:59 +0900

    BaekJoon1956_v1 excercise

---
+++ CodingTest/coding/src/shortestPath/BaekJoon1956_v1.java
@@ -0,0 +1,58 @@
+package shortestPath;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BaekJoon1956_v1 {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1<<16);
+    static final int INF = 4000001;
+
+    public static void main(String[] args) throws Exception {
+        new BaekJoon1956_v1().solution();
+    }
+
+    private void solution() throws Exception {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int v = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[v+1][v+1];
+        for (int[] row : arr) Arrays.fill(row, INF);
+        while (e-->0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            arr[a][b] = c;
+        }
+
+
+        for (int k = 1; k <= v; k++) {
+            for (int i = 1; i <= v; i++) {
+                for (int j = 1; j <= v; j++) {
+
+                    arr[i][j] = Math.min(arr[i][k] + arr[k][j], arr[i][j]);
+
+                }
+            }
+        }
+
+//        for (int i=1; i<=v; i++) {
+//            for (int j=1; j<=v; j++) {
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+
+        int answer = INF;
+        for (int i = 1; i <= v; i++) {
+            answer = Math.min(answer, arr[i][i]);
+        }
+
+        System.out.println(answer == INF ? -1 : answer);
+    }
+}
