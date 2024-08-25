888bfca2fcd136c51fc3719d01b2a8a68434fa20
focandlol <focandlol@naver.com>
2024-08-26 07:43:17 +0900

    BaekJoon9063 earth

---
+++ CodingTest/coding/src/BaekJoon9063.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon9063 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int minX = 10001;
+        int minY = 10001;
+        int maxX = -10001;
+        int maxY = -10001;
+
+        for(int i = 0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+            maxX = Math.max(maxX, x);
+            maxY = Math.max(maxY, y);
+        }
+
+        System.out.println((maxX - minX) * (maxY - minY));
+    }
+}
