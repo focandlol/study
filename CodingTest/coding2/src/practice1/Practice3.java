d96f0893ce1955b8992309351be19cab1fc3b8ec
focandlol <focandlol@naver.com>
2024-08-27 13:47:28 +0900

    practice3

---
+++ CodingTest/coding2/src/practice1/Practice3.java
@@ -0,0 +1,43 @@
+package practice1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Practice3 {
+    static boolean[] visited;
+    static char[] dap;
+    static String s1;
+    static String s2;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        s1 = br.readLine();
+        s2 = br.readLine();
+
+        visited = new boolean[s1.length()];
+        dap = new char[s1.length()];
+
+        find(0);
+
+        System.out.println(false);
+    }
+
+    private static void find(int depth) {
+        if(depth == s1.length()) {
+            String s = String.valueOf(dap);
+            if(s2.contains(s)) {
+                System.out.println(true);
+                System.exit(0);
+            }
+            return;
+        }
+        for(int i=0; i<s1.length(); i++) {
+            if(visited[i]) continue;
+            visited[i] = true;
+            dap[depth] = s1.charAt(i);
+            find(depth+1);
+            visited[i] = false;
+        }
+    }
+}
