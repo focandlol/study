14ac277095d44ca963d688296579a2a1128ed0fa
focandlol <focandlol@naver.com>
2024-08-27 12:11:30 +0900

    practice 1 pascal's triangle

---
+++ CodingTest/coding2/src/practice1/Practice1.java
@@ -0,0 +1,29 @@
+package practice1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        ArrayList<Integer>[] list = new ArrayList[n];
+        for(int i=0; i<n; i++){
+            list[i] = new ArrayList<>();
+
+            for(int j=0; j<i+1; j++){
+                if(j == 0 || j == i){
+                    list[i].add(1);
+                }else{
+                    list[i].add(list[i-1].get(j-1) + list[i-1].get(j));
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(list));
+
+    }
+
+}
