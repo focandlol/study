509d7596823a3739b07eb877dda3df9dd01d7722
focandlol <focandlol@naver.com>
2024-09-04 16:51:41 +0900

    array practice5

---
+++ CodingTest/coding2/src/array/Practice5.java
@@ -0,0 +1,12 @@
+package array;
+
+import java.util.Arrays;
+
+public class Practice5 {
+    public static void main(String[] args) {
+        int[] arr = {5,3,1,4,6,1};
+
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
