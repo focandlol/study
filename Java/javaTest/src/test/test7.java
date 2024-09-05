e996fb2d02afc51d891e76f4e129a85c70775785
focandlol <focandlol@naver.com>
2024-09-05 12:27:12 +0900

    class1,2

--- Java/javaTest/src/test/test7.java
+++ Java/javaTest/src/test/test7.java
@@ -12,89 +12,82 @@ public class test7 {
 
         System.out.print("로또 개수를 입력해 주세요.(숫자 1~10):");
         int num = sc.nextInt();
-        HashMap<Character,int[]> map = new HashMap<>();
+        HashMap<Character, int[]> map = new HashMap<>();
 
         char start = 'A';
-        Lotto lotto = new Lotto();
-        for(int i=0; i<num; i++){
-            int[] arr = lotto.create();
-            map.put(start,arr);
-            System.out.printf("%c\t%s\n",start,lotto.print(arr));
+        Random r = new Random();
+        for (int i = 0; i < num; i++) {
+            int[] arr = create(r);
+            map.put(start, arr);
+            System.out.printf("%c\t%s\n", start, print(arr));
             start++;
         }
         System.out.println();
 
         int[] dap;
         System.out.println("[로또 발표]");
-        dap = lotto.create();
-        System.out.printf("\t%s\n",lotto.print(dap));
+        dap = create(r);
+        System.out.printf("\t%s\n", print(dap));
         System.out.println();
 
         System.out.println("[내 로또 결과]");
-        lotto.printResult(map,dap);
+        printResult(map, dap);
 
     }
 
-    static class Lotto{
-        Random random;
 
-        public Lotto() {
-            this.random = new Random();
+    static int[] create(Random random) {
+        int[] arr = new int[6];
+        for (int i = 0; i < 6; i++) {
+            arr[i] = random.nextInt(45) + 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] == arr[j]) {
+                    i--;
+                    break;
+                }
+            }
         }
+        Arrays.sort(arr);
+        return arr;
+    }
 
-        public int[] create(){
-            int[] arr = new int[6];
-            for(int i=0; i<6; i++){
-                arr[i] = random.nextInt(45) + 1;
-                for(int j=0; j<i; j++){
-                    if(arr[i] == arr[j]){
-                        i--;
+    static String print(int[] arr) {
+        StringBuffer sf = new StringBuffer();
+        for (int i = 0; i < arr.length; i++) {
+            if (i != 0) {
+                sf.append(",");
+            }
+            if (arr[i] < 10) {
+                sf.append("0" + arr[i]);
+            } else {
+                sf.append(arr[i]);
+            }
+        }
+        return sf.toString();
+    }
+
+    static void printResult(HashMap<Character, int[]> map, int[] dap) {
+        char al = 'A';
+        for (int i = 0; i < map.size(); i++) {
+            int[] arr = map.get(al);
+            int count = 0;
+            for (int j = 0; j < dap.length; j++) {
+                int start = 0;
+                int end = arr.length - 1;
+                while (start <= end) {
+                    int mid = (start + end) / 2;
+                    if (arr[mid] == dap[j]) {
+                        count++;
                         break;
+                    } else if (dap[j] < arr[mid]) {
+                        end = mid - 1;
+                    } else if (dap[j] > arr[mid]) {
+                        start = mid + 1;
                     }
                 }
             }
-            Arrays.sort(arr);
-            return arr;
+            System.out.printf("%c\t%s => %d개 일치\n", al, print(arr), count);
+            al++;
         }
-
-        public String print(int[] arr){
-            StringBuffer sf = new StringBuffer();
-            for(int i=0; i<arr.length; i++){
-                if(i != 0){
-                    sf.append(",");
-                }
-                if(arr[i] < 10) {
-                    sf.append("0"+arr[i]);
-                }else{
-                    sf.append(arr[i]);
-                }
-            }
-            return sf.toString();
-        }
-
-       public void printResult(HashMap<Character,int[]> map, int[] dap){
-            char al = 'A';
-            for(int i=0; i<map.size(); i++){
-                int[] arr = map.get(al);
-                int count = 0;
-                for(int j = 0; j<dap.length; j++) {
-                    int start = 0;
-                    int end = arr.length - 1;
-                    while (start <= end) {
-                        int mid = (start + end) / 2;
-                        if(arr[mid] == dap[j]){
-                            count++;
-                            break;
-                        }else if(dap[j] < arr[mid]){
-                            end = mid - 1;
-                        }else if(dap[j] > arr[mid]){
-                            start = mid + 1;
-                        }
-                    }
-                }
-                System.out.printf("%c\t%s => %d개 일치\n", al, print(arr), count);
-                al++;
-            }
-       }
     }
 }
