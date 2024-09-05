aa31ac82bdc6110cd40ca8b56da8b8c642df1af4
focandlol <focandlol@naver.com>
2024-09-05 12:27:12 +0900

    class1,2

---
+++ Java/Javaz/src/classs/Practice.java
@@ -0,0 +1,26 @@
+package classs;
+
+class Calculator {
+    public int sum(int a, int b) {
+        return a +b;
+    }
+    public double sum(double a, double b) {
+        return a+b;
+    }
+    public double sum(String a, String b) {
+        return Double.parseDouble(a) + Double.parseDouble(b);
+    }
+    public int sum(int a, int b, int c) {
+        return a+b+c;
+    }
+}
+
+public class Practice {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        System.out.println(c.sum(1, 2));
+        System.out.println(c.sum(1.0,2.0));
+        System.out.println(c.sum("1","2"));
+        System.out.println(c.sum(1,2,3));
+    }
+}
--- Java/javaTest/src/test/test6.java
+++ Java/javaTest/src/test/test6.java
@@ -10,68 +10,60 @@ public class test6 {
         System.out.print("가상 선거를 진행할 후보자 인원을 입력해 주세요.");
         int num = sc.nextInt();
 
-        HashMap<Integer,Pe> map = new HashMap<>();
-        for(int i=1; i<=num; i++){
+        HashMap<Integer, Pe> map = new HashMap<>();
+        for (int i = 1; i <= num; i++) {
             System.out.print(i + "번째 후보자이름을 입력해 주세요.");
-            map.put(i,new Pe(sc.next()));
+            map.put(i, new Pe(sc.next()));
         }
         System.out.println();
 
-        Vote vote = new Vote(map,total);
-        vote.vote();
-        vote.result();
+        Random rand = new Random();
+        vote(total, rand, map);
+        result(map);
 
 
     }
-    static class Vote{
-        private HashMap<Integer,Pe> map;
-        private int total;
-        Random rand;
 
-        public Vote(HashMap<Integer, Pe> map, int total) {
-            this.map = map;
-            this.total = total;
-            this.rand = new Random();
+    static void vote(int total, Random rand, HashMap<Integer, Pe> map) {
+        for (int i = 1; i <= total; i++) {
+            int r = rand.nextInt(map.size()) + 1;
+            map.get(r).count++;
+            print(i, r, total, map);
         }
+    }
 
-        public void vote(){
-            for(int i=1; i<=total; i++){
-                int r = rand.nextInt(map.size()) + 1;
-                map.get(r).count++;
-                print(i,r);
-            }
+    static void print(int i, int r, int total, HashMap<Integer, Pe> map) {
+        System.out.printf("[투표진행률]: %.2f%%, %d명 투표 => %s\n", (double) i * 100 / total, i, map.get(r).name);
+        for (int j = 1; j <= map.size(); j++) {
+            System.out.printf("[기호:%d] %s:\t%.2f%%\t(투표수: %d)\n", j, map.get(j).name,
+                    (double) map.get(j).count * 100.0 / total, map.get(j).count);
         }
+        System.out.println();
+    }
 
-        public void print(int i,int r){
-            System.out.printf("[투표진행률]: %.2f%%, %d명 투표 => %s\n",(double)i*100/total, i,map.get(r).name);
-            for(int j=1; j<=map.size(); j++){
-                System.out.printf("[기호:%d] %s:\t%.2f%%\t(투표수: %d)\n", j, map.get(j).name,
-                        (double)map.get(j).count * 100.0 / total, map.get(j).count);
-            }
-            System.out.println();
-        }
+    static void result(HashMap<Integer, Pe> map) {
+        ArrayList<Integer> list = new ArrayList<>(map.keySet());
+        list.sort((a, b) -> map.get(b).count - map.get(a).count);
 
-        public void result() {
-            ArrayList<Integer> list = new ArrayList<>(map.keySet());
-            list.sort((a,b) -> map.get(b).count - map.get(a).count);
-
-            if(map.get(list.get(0)).count == map.get(list.get(1)).count){
-                System.out.print("[투표결과] 동률 : ");
-                for(int i=0; i<list.size(); i++){
-                    if(map.get(list.get(0)).count == map.get(list.get(i)).count){
-                        System.out.print(map.get(list.get(i)).name);
-                    }else{
-                        break;
-                    }
+        if (map.get(list.get(0)).count == map.get(list.get(1)).count) {
+            System.out.print("[투표결과] 동률 : ");
+            for (int i = 0; i < list.size(); i++) {
+                if(i != 0) {
                     System.out.print(",");
                 }
-            }else{
-                System.out.printf("[투표결과] 당선인 : %s",map.get(list.get(0)).name);
+                if (map.get(list.get(0)).count == map.get(list.get(i)).count) {
+                    System.out.print(map.get(list.get(i)).name);
+                } else {
+                    break;
+                }
             }
+        } else {
+            System.out.printf("[투표결과] 당선인 : %s", map.get(list.get(0)).name);
         }
     }
 
-    static class Pe{
+
+    static class Pe {
         String name;
         int count;
 
