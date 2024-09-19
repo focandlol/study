8dbf74747828b80f6a9e46c9e80e6361985de2c4
focandlol <focandlol@naver.com>
2024-09-19 13:45:01 +0900

    26008

--- CodingTest/coding2/src/Math/BaekJoon26008_kkkkkk.java
+++ CodingTest/coding2/src/Math/BaekJoon26008_kkkkkk.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BaekJoon26008 {
+public class BaekJoon26008_kkkkkk {
     static final int na = 1000000007;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
