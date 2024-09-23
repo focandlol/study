70cac566b73dbd969ccdf95c88970a7f856453a1
focandlol <focandlol@naver.com>
2024-09-23 12:02:39 +0900

    sunh p2_1 practice2 ver2

---
+++ CodingTest/coding2/src/queue/p2_1/Practice2ver2.java
@@ -0,0 +1,40 @@
+package queue.p2_1;
+
+import java.util.Stack;
+
+public class Practice2ver2 {
+    public static void main(String[] args) {
+        int[] nums = {4,3,6,8,7,5,2,1};
+        solution(nums);
+
+        nums = new int[]{1,2,5,3,4};
+        solution(nums);
+
+//        nums = new int[]{4,3,2,1,1};
+//        solution(nums);
+    }
+
+    private static void solution(int[] nums) {
+        Stack<Integer> stack = new Stack<Integer>();
+
+        int idx = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nums.length; i++) {
+            if(idx < nums[i]){
+                for(int j = idx+1; j<= nums[i]; j++){
+                    stack.push(j);
+                    sb.append("+").append(" ");
+                }
+                idx = nums[i];
+            }else if(idx > nums[i]){
+                if(!stack.isEmpty() && stack.peek() != nums[i]){
+                    System.out.println("NO");
+                    return;
+                }
+            }
+            stack.pop();
+            sb.append("-").append(" ");
+        }
+        System.out.println(sb);
+    }
+}
