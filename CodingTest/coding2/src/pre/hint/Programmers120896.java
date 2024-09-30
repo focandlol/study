6b80680251a7209dbde93206138a14b199476b78
focandlol <focandlol@naver.com>
2024-09-30 10:27:04 +0900

    Programmers120896

---
+++ CodingTest/coding2/src/pre/hint/Programmers120896.java
@@ -0,0 +1,21 @@
+package pre.hint;
+
+import java.util.*;
+import java.util.stream.*;
+
+public class Programmers120896 {
+    static class Solution {
+       static String solution(String s) {
+            String a = "";
+            Map<Character, Long> collect = IntStream.range(0, s.length()).mapToObj(i -> s.charAt(i))
+                    .collect(Collectors.groupingBy(q -> q, Collectors.counting()));
+
+            a = collect.keySet().stream().filter(t -> collect.get(t) == 1).sorted().map(b -> String.valueOf(b)).collect(Collectors.joining());
+            return a;
+        }
+    }
+    public static void main(String[] args) {
+       String a = "asdas";
+       System.out.println(Solution.solution(a));
+    }
+}
