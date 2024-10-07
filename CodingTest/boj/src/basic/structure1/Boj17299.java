5685efda703789fa1ae7dfc9ed96b1d5842f6955
focandlol <focandlol@naver.com>
2024-10-07 12:30:52 +0900

    basic structure1 Boj17299 odeungkeunsu

---
+++ CodingTest/boj/src/basic/structure1/Boj17299.java
@@ -0,0 +1,42 @@
+package basic.structure1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Boj17299 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        int[] count = new int[1000001];
+        Stack<Integer> stack = new Stack<Integer>();
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            count[arr[i]]++;
+        }
+
+        for(int i=0; i<n; i++){
+            while(!stack.isEmpty() && count[arr[stack.peek()]] < count[arr[i]]){
+                arr[stack.pop()] = arr[i];
+            }
+            stack.push(i);
+        }
+
+        while(!stack.isEmpty()){
+            arr[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++){
+            sb.append(arr[i]).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
