ad4b806f0075f22a1c72ac7f034be470f64f53e3
focandlol <focandlol@naver.com>
2024-10-09 10:53:14 +0900

    basic structure1 Boj2743

---
+++ CodingTest/boj/src/basic/structure1/Boj2743.java
@@ -0,0 +1,9 @@
+package basic.structure1;
+
+import java.util.Scanner;
+
+public class Boj2743 {
+    public static void main(String[] args) {
+        System.out.println(new Scanner(System.in).next().length());
+    }
+}
