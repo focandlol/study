29902935143503f7c08b16aea78a05a786bd807f
focandlol <focandlol@naver.com>
2024-10-12 12:15:58 +0900

    basic dynamic Boj11052 buy card

---
+++ CodingTest/boj/src/basic/dynamic/Boj11052.java
@@ -0,0 +1,29 @@
+package basic.dynamic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj11052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n+1];
+        int[] dap = new int[n+1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                dap[i] = Math.max(arr[j] + dap[i-j], dap[i]);
+            }
+        }
+        System.out.println(dap[n]);
+    }
+}
