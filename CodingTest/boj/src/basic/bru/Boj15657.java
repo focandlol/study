d48833005c59f30c76f7bd58a3e2815e0fb72d75
focandlol <focandlol@naver.com>
2024-10-23 10:34:11 +0900

    basic bru Boj15657 n&m 8

---
+++ CodingTest/boj/src/basic/bru/Boj15657.java
@@ -0,0 +1,47 @@
+package basic.bru;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj15657 {
+    static int n;
+    static int m;
+    static int[] arr;
+    static int[] dep;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        dep = new int[m];
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        back(0,0);
+        System.out.println(sb);
+    }
+
+    private static void back(int depth, int start) {
+        if(depth == m) {
+            for(int i=0; i<m; i++) {
+                sb.append(dep[i] + " ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for(int i=start; i<n; i++) {
+            dep[depth] = arr[i];
+            back(depth+1, i);
+        }
+    }
+}
