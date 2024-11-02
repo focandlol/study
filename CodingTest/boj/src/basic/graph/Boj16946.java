aa597aed451b9bf4710c1b241a9eb8cbe6350014
focandlol <focandlol@naver.com>
2024-11-02 12:44:20 +0900

    basic graph Boj16946

---
+++ CodingTest/boj/src/basic/graph/Boj16946.java
@@ -0,0 +1,75 @@
+package basic.graph;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Boj16946 {
+    static int[][] di = {{0,1,0,-1},{1,0,-1,0}};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        char[][] arr = new char[n][m];
+        int[][] group = new int[n][m];
+        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+
+        for(int i=0; i<n; i++) {
+            arr[i] = br.readLine().toCharArray();
+        }
+
+        int idx = 1;
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(arr[i][j] == '0' && group[i][j] == 0) {
+                    int count = 1;
+                    group[i][j] = idx;
+                    Queue<int[]> q = new LinkedList<>();
+                    q.add(new int[]{i, j});
+                    while(!q.isEmpty()) {
+                        int[] cur = q.poll();
+                        for(int k=0; k<4; k++){
+                            int x = cur[0] + di[0][k];
+                            int y = cur[1] + di[1][k];
+                            if(x >= 0 && x <n && y >= 0 && y < m && arr[x][y] == '0' && group[x][y] == 0) {
+                                group[x][y] = idx;
+                                count++;
+                                q.add(new int[]{x, y});
+                            }
+                        }
+                    }
+                    map.put(idx,count);
+                    idx++;
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                int count = 0;
+                if(arr[i][j] == '1') {
+                    count++;
+                    HashSet<Integer> set = new HashSet<Integer>();
+                    for(int k=0; k<4; k++){
+                        int x = i + di[0][k];
+                        int y = j + di[1][k];
+                        if(x >=0 && x<n && y >= 0 && y < m && arr[x][y] == '0' && group[x][y] != 0) {
+                            if(set.add(group[x][y])) {
+                                count += map.get(group[x][y]);
+                            }
+                        }
+                    }
+                }
+                sb.append(count % 10);
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
---
+++ Java/wifiProject/src/main/java/org/example/service/WifiService.java
@@ -0,0 +1,179 @@
+package org.example.service;
+
+import org.example.dto.WifiDto;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.example.db.Db.close;
+import static org.example.db.Db.getConnection;
+import static org.example.service.HistoryService.addHistory;
+
+public class WifiService {
+    public static int dbInsert(JSONArray jsonArray){
+        ResultSet rs = null;
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        int count = 0;
+        try{
+            connection = getConnection();
+            connection.setAutoCommit(false);
+            String sql = " insert into wifi "
+                    + " ( x_swifi_mgr_no, x_swifi_wrdofc, x_swifi_main_nm, x_swifi_adres1, x_swifi_adres2, "
+                    + " x_swifi_instl_floor, x_swifi_instl_ty, x_swifi_instl_mby, x_swifi_svc_se, x_swifi_cmcwr, "
+                    + " x_swifi_cnstc_year, x_swifi_inout_door, x_swifi_remars3, lat, lnt, work_dttm) "
+                    + " values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?); ";
+            pstmt = connection.prepareStatement(sql);
+
+            for (int i = 0; i < jsonArray.size(); i++) {
+
+               // System.out.println("memberService1");
+                JSONObject data = (JSONObject) jsonArray.get(i);
+               // System.out.println("memberService2");
+                System.out.println(data.get("LAT"));
+                pstmt.setString(1, (String) data.get("X_SWIFI_MGR_NO"));
+                pstmt.setString(2, (String) data.get("X_SWIFI_WRDOFC"));
+                pstmt.setString(3, (String) data.get("X_SWIFI_MAIN_NM"));
+                pstmt.setString(4, (String) data.get("X_SWIFI_ADRES1"));
+                pstmt.setString(5, (String) data.get("X_SWIFI_ADRES2"));
+                pstmt.setString(6, (String) data.get("X_SWIFI_INSTL_FLOOR"));
+                pstmt.setString(7, (String) data.get("X_SWIFI_INSTL_TY"));
+                pstmt.setString(8, (String) data.get("X_SWIFI_INSTL_MBY"));
+                pstmt.setString(9, (String) data.get("X_SWIFI_SVC_SE"));
+                pstmt.setString(10, (String) data.get("X_SWIFI_CMCWR"));
+                pstmt.setString(11, (String) data.get("X_SWIFI_CNSTC_YEAR"));
+                pstmt.setString(12, (String) data.get("X_SWIFI_INOUT_DOOR"));
+                pstmt.setString(13, (String) data.get("X_SWIFI_REMARS3"));
+                pstmt.setString(14, (String) data.get("LAT"));
+                pstmt.setString(15, (String) data.get("LNT"));
+                pstmt.setString(16, (String) data.get("WORK_DTTM"));
+                System.out.println("memberService3");
+                pstmt.addBatch();
+                pstmt.clearParameters();
+
+                if ((i + 1) % 1000 == 0) {
+                    int[] result = pstmt.executeBatch();
+                    count += result.length;    //배치한 완료 개수
+                    connection.commit();
+                }
+            }
+
+            int[] result = pstmt.executeBatch();
+            count += result.length;    //배치한 완료 개수
+            connection.commit();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally{
+            close(rs,pstmt,connection);
+        }
+        System.out.println("memberService4");
+        return count;
+    }
+
+    public List<WifiDto> getNearestWifiList(String lat, String lnt) {
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        List<WifiDto> wifiDtoList = new ArrayList<WifiDto>();
+        try{
+            connection = getConnection();
+
+            String sql = "SELECT *, " +
+                    "ROUND(6371 * ACOS(COS(RADIANS(?)) * COS(RADIANS(lat)) * COS(RADIANS(lnt) - RADIANS(?)) + SIN(RADIANS(?)) * SIN(RADIANS(lat))), 4) AS distance " +
+                    "FROM " +
+                    "    wifi " +
+                    "ORDER BY " +
+                    "    distance " +
+                    "LIMIT 20;";
+            pstmt = connection.prepareStatement(sql);
+            pstmt.setDouble(1, Double.parseDouble(lat));
+            pstmt.setDouble(2, Double.parseDouble(lnt));
+            pstmt.setDouble(3, Double.parseDouble(lat));
+
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                WifiDto wifiDto = WifiDto.builder()
+                        .wifiId(rs.getInt("wifi_id"))
+                        .xSwifiMgrNo(rs.getString("x_swifi_mgr_no"))
+                        .xSwifiWrdofc(rs.getString("x_swifi_wrdofc"))
+                        .xSwifiMainNm(rs.getString("x_swifi_main_nm"))
+                        .xSwifiAdres1(rs.getString("x_swifi_adres1"))
+                        .xSwifiAdres2(rs.getString("x_swifi_adres2"))
+                        .xSwifiInstlFloor(rs.getString("x_swifi_instl_floor"))
+                        .xSwifiInstlTy(rs.getString("x_swifi_instl_ty"))
+                        .xSwifiInstlMby(rs.getString("x_swifi_instl_mby"))
+                        .xSwifiSvcSe(rs.getString("x_swifi_svc_se"))
+                        .xSwifiCmcwr(rs.getString("x_swifi_cmcwr"))
+                        .xSwifiCnstcYear(rs.getString("x_swifi_cnstc_year"))
+                        .xSwifiInoutDoor(rs.getString("x_swifi_inout_door"))
+                        .xSwifiRemars3(rs.getString("x_swifi_remars3"))
+                        .lat(rs.getString("lat"))
+                        .lnt(rs.getString("lnt"))
+                        .workDttm(String.valueOf(rs.getTimestamp("work_dttm").toLocalDateTime()))
+                        .distance(rs.getDouble("distance"))
+                        .build();
+                wifiDtoList.add(wifiDto);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally{
+            close(rs,pstmt,connection);
+        }
+        addHistory(lat,lnt);
+        return wifiDtoList;
+    }
+    public WifiDto getDetailWifi(String id, String distance) {
+        Connection connection = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        WifiDto wifiDto = null;
+        try{
+            connection = getConnection();
+
+            String sql = "select * from wifi where wifi_id=?";
+            pstmt = connection.prepareStatement(sql);
+            pstmt.setInt(1, Integer.parseInt(id));
+
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                    wifiDto = WifiDto.builder()
+                        .wifiId(rs.getInt("wifi_id"))
+                        .xSwifiMgrNo(rs.getString("x_swifi_mgr_no"))
+                        .xSwifiWrdofc(rs.getString("x_swifi_wrdofc"))
+                        .xSwifiMainNm(rs.getString("x_swifi_main_nm"))
+                        .xSwifiAdres1(rs.getString("x_swifi_adres1"))
+                        .xSwifiAdres2(rs.getString("x_swifi_adres2"))
+                        .xSwifiInstlFloor(rs.getString("x_swifi_instl_floor"))
+                        .xSwifiInstlTy(rs.getString("x_swifi_instl_ty"))
+                        .xSwifiInstlMby(rs.getString("x_swifi_instl_mby"))
+                        .xSwifiSvcSe(rs.getString("x_swifi_svc_se"))
+                        .xSwifiCmcwr(rs.getString("x_swifi_cmcwr"))
+                        .xSwifiCnstcYear(rs.getString("x_swifi_cnstc_year"))
+                        .xSwifiInoutDoor(rs.getString("x_swifi_inout_door"))
+                        .xSwifiRemars3(rs.getString("x_swifi_remars3"))
+                        .lat(rs.getString("lat"))
+                        .lnt(rs.getString("lnt"))
+                        .workDttm(String.valueOf(rs.getTimestamp("work_dttm").toLocalDateTime()))
+                        .distance(Double.parseDouble(distance))
+                        .build();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally{
+            close(rs,pstmt,connection);
+        }
+        return wifiDto;
+    }
+}
