24898c8435e6a565c93859ac33593f3362596f36
focandlol <focandlol@naver.com>
2024-11-11 18:46:24 +0900

    basic graph2 Boj1197

---
+++ Spring/WebSample/src/main/java/org/example/websample/config/WebConfig.java
@@ -0,0 +1,30 @@
+package org.example.websample.config;
+
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public FilterRegistrationBean loggingFilter(){
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/*", "/images/*");
+    }
+}
