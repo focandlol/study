3d6859cc051ed38518836415018cf018caf6ecc1
focandlol <focandlol@naver.com>
2024-11-13 13:52:58 +0900

    lock test

--- Spring/Account/src/main/java/org/example/account/controller/AccountController.java
+++ Spring/Account/src/main/java/org/example/account/controller/AccountController.java
@@ -3,6 +3,7 @@ package org.example.account.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.account.domain.Account;
 import org.example.account.service.AccountService;
+import org.example.account.service.RedisTestService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
+    private final RedisTestService redisTestService;
+
+    @GetMapping("/get-lock")
+    public String getLock() {
+        return redisTestService.getLock();
+    }
 
     @GetMapping("/create-account")
     public String createAccount() {
