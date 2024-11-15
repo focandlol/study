0d9c6180cc351128ebcbfe8365e157548b8c756f
focandlol <focandlol@naver.com>
2024-11-15 14:09:43 +0900

    transaction

---
+++ Spring/Account/src/main/java/org/example/account/controller/TransactionController.java
@@ -0,0 +1,41 @@
+package org.example.account.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.account.dto.TransactionDto;
+import org.example.account.dto.UseBalance;
+import org.example.account.exception.AccountException;
+import org.example.account.service.TransactionService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.security.auth.login.AccountNotFoundException;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @PostMapping("/transaction/use")
+    public UseBalance.Response useBalance(
+            @Valid @RequestBody UseBalance.Request request
+    ){
+        try {
+            return UseBalance.Response.from(transactionService.useBalance(request.getUserId(),
+                    request.getAccountNumber(), request.getAmount()));
+        } catch(AccountException e) {
+            log.error("Failed to use balance");
+
+            transactionService.saveFailedUseTransaction(
+                    request.getAccountNumber(),
+                    request.getAmount()
+            );
+            throw e;
+        }
+    }
+
+}
