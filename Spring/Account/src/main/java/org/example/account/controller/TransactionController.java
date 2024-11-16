983bc4cce6898dec99bd709d00e3d4ccb40804e8
focandlol <focandlol@naver.com>
2024-11-16 18:36:43 +0900

    queryTransaction, test

--- Spring/Account/src/main/java/org/example/account/controller/TransactionController.java
+++ Spring/Account/src/main/java/org/example/account/controller/TransactionController.java
@@ -4,13 +4,12 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.account.dto.CancelBalance;
+import org.example.account.dto.QueryTransactionResponse;
 import org.example.account.dto.TransactionDto;
 import org.example.account.dto.UseBalance;
 import org.example.account.exception.AccountException;
 import org.example.account.service.TransactionService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.security.auth.login.AccountNotFoundException;
 
@@ -55,6 +54,11 @@ public class TransactionController {
             );
             throw e;
         }
+    }
+
+    @GetMapping("/transaction/{transactionId}")
+    public QueryTransactionResponse queryTransaction(@PathVariable String transactionId){
+        return QueryTransactionResponse.from(transactionService.queryTransaction(transactionId));
     }
 
 
