5b7b15cb38bcbe0d1808ea7cc340618939a4f214
focandlol <focandlol@naver.com>
2024-11-19 18:04:52 +0900

    jdbc test

---
+++ Spring/weather/src/main/java/focandlol/weather/domain/Memo.java
@@ -0,0 +1,15 @@
+package focandlol.weather.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Memo {
+    private int id;
+    private String text;
+}
