5bb6a5d8b846f0cfb0bc4876618144f72caf3500
focandlol <focandlol@naver.com>
2024-11-20 13:33:20 +0900

    get weather data from openweather

---
+++ Spring/weather/src/main/java/focandlol/weather/repository/DiaryRepository.java
@@ -0,0 +1,10 @@
+package focandlol.weather.repository;
+
+import focandlol.weather.domain.Diary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+//public interface DiaryRepository extends JpaRepository<Diary,Integer> {
+//
+//
+//}
