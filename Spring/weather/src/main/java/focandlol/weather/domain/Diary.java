7a283b313b18de39a8814c10ea4cee624d046a21
focandlol <focandlol@naver.com>
2024-11-20 19:00:17 +0900

    insert db

--- Spring/weather/src/main/java/focandlol/weather/domain/Diary.java
+++ Spring/weather/src/main/java/focandlol/weather/domain/Diary.java
@@ -1,4 +1,29 @@
 package focandlol.weather.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Diary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String weather;
+    private String icon;
+    private double temperature;
+    private String text;
+    private LocalDate date;
 }
--- Spring/weather/src/main/java/focandlol/weather/repository/DiaryRepository.java
+++ Spring/weather/src/main/java/focandlol/weather/repository/DiaryRepository.java
@@ -2,9 +2,6 @@ package focandlol.weather.repository;
 
 import focandlol.weather.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
 
-//public interface DiaryRepository extends JpaRepository<Diary,Integer> {
-//
-//
-//}
+public interface DiaryRepository extends JpaRepository<Diary,Integer> {
+}
