4759d1a1d8df3a46983631ae3e2b0c609693990f
focandlol <focandlol@naver.com>
2024-11-21 11:36:03 +0900

    update diary

--- Spring/weather/src/main/java/focandlol/weather/repository/DiaryRepository.java
+++ Spring/weather/src/main/java/focandlol/weather/repository/DiaryRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface DiaryRepository extends JpaRepository<Diary,Integer> {
 
     List<Diary> findAllByDate(LocalDate date);
 
     List<Diary> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
+
+    Diary getFirstByDate(LocalDate date);
 }
