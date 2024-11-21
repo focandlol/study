1e690bd97e8a02cc283bd87df38bb313a4aeaa80
focandlol <focandlol@naver.com>
2024-11-21 11:52:30 +0900

    delete diary

--- Spring/weather/src/main/java/focandlol/weather/repository/DiaryRepository.java
+++ Spring/weather/src/main/java/focandlol/weather/repository/DiaryRepository.java
@@ -14,4 +14,7 @@ public interface DiaryRepository extends JpaRepository<Diary,Integer> {
     List<Diary> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
 
     Diary getFirstByDate(LocalDate date);
+
+    void deleteAllByDate(LocalDate date);
+
 }
