0e643fb3101c2ebc6bfd4e15628c3c12f2e7e260
focandlol <focandlol@naver.com>
2024-11-26 17:53:30 +0900

    scra

---
+++ Spring/Dividends/src/test/java/focandlol/dividends/DividendsApplicationTests.java
@@ -0,0 +1,13 @@
+package focandlol.dividends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DividendsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
