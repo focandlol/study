c3ab76416f777a0590d1b931a8a077dba4e78c0a
focandlol <focandlol@naver.com>
2024-11-27 13:37:07 +0900

    autocomplete

--- Spring/Dividends/src/main/java/focandlol/dividends/web/CompanyController.java
+++ Spring/Dividends/src/main/java/focandlol/dividends/web/CompanyController.java
@@ -21,7 +21,9 @@ public class CompanyController {
 
     @GetMapping("/autocomplete")
     public ResponseEntity<?> autocomplete(@RequestParam String keyword) {
-        return null;
+        //List<String> result = companyService.autocomplete(keyword);
+        List<String> result = companyService.getCompanyNamesByKeyword(keyword);
+        return ResponseEntity.ok(result);
     }
 
     @GetMapping
@@ -37,6 +39,7 @@ public class CompanyController {
             throw new RuntimeException("ticker is empty");
         }
         Company company = companyService.save(ticker);
+        companyService.addAutocompleteKeyword(company.getName());
         return ResponseEntity.ok(company);
     }
 
