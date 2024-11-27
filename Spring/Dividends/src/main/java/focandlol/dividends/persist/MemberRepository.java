e561b03444a050551652fc7dd0452dd887bed259
focandlol <focandlol@naver.com>
2024-11-27 20:38:31 +0900

    signUp

---
+++ Spring/Dividends/src/main/java/focandlol/dividends/persist/MemberRepository.java
@@ -0,0 +1,13 @@
+package focandlol.dividends.persist;
+
+import focandlol.dividends.model.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+
+    Optional<MemberEntity> findByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
---
+++ Spring/Dividends/src/main/java/focandlol/dividends/service/MemberService.java
@@ -0,0 +1,43 @@
+package focandlol.dividends.service;
+
+import focandlol.dividends.model.Auth;
+import focandlol.dividends.model.MemberEntity;
+import focandlol.dividends.persist.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class MemberService implements UserDetailsService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("couldn't find user -> " + username));
+    }
+
+    public MemberEntity register(Auth.SignUp member) {
+        boolean exists = memberRepository.existsByUsername(member.getUsername());
+        if(exists){
+            throw new RuntimeException("이미 사용 중인 아이디 입니다");
+        }
+
+        member.setPassword(passwordEncoder.encode(member.getPassword()));
+        MemberEntity result = memberRepository.save(member.toEntity());
+
+        return result;
+    }
+
+    public MemberEntity authenticate(Auth.SignIn signIn){
+        return null;
+    }
+}
