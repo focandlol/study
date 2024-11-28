03edaec29d29072985c746de243012e6318d9796
focandlol <focandlol@naver.com>
2024-11-28 11:21:51 +0900

    jwt

--- Spring/Dividends/src/main/java/focandlol/dividends/security/TokenProvider.java
+++ Spring/Dividends/src/main/java/focandlol/dividends/security/TokenProvider.java
@@ -1,4 +1,58 @@
 package focandlol.dividends.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class TokenProvider {
+
+    private static final long TOKEN_EXPIRE_TIME = 1000 * 60 * 60; // 1시간
+    private static final String KEY_ROLES = "roles";
+    @Value("${spring.jwt.secret}")
+    private String secretKey;
+
+    public String generateToken(String username, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(KEY_ROLES, roles);
+
+        Date now = new Date();
+        Date expireDate = new Date(now.getTime() + TOKEN_EXPIRE_TIME);
+
+        return Jwts.builder().setClaims(claims)
+                .setIssuedAt(now) // 토큰 생성 시간
+                .setExpiration(expireDate) // 토큰 만료 시간
+                .signWith(SignatureAlgorithm.HS512,secretKey) // 사용할 암호화 알고리즘, 비밀키
+                .compact();
+    }
+
+    public String getUsername(String token) {
+        return parseClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        if(!StringUtils.hasText(token)){
+            return false;
+        }
+
+        Claims claims = parseClaims(token);
+        return !claims.getExpiration().before(new Date());
+    }
+
+    private Claims parseClaims(String token) {
+        try {
+            return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+        } catch(ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
 }
