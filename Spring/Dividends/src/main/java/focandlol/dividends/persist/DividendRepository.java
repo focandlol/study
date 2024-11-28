a753f25c5aa26d645f9df46ccb4a9d6ac06e8e7d
focandlol <focandlol@naver.com>
2024-11-28 13:15:35 +0900

    delete

--- Spring/Dividends/src/main/java/focandlol/dividends/persist/DividendRepository.java
+++ Spring/Dividends/src/main/java/focandlol/dividends/persist/DividendRepository.java
@@ -2,6 +2,9 @@ package focandlol.dividends.persist;
 
 import focandlol.dividends.persist.entity.DividendEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,4 +13,8 @@ public interface DividendRepository extends JpaRepository<DividendEntity,Long> {
     List<DividendEntity> findAllByCompanyId(Long companyId);
 
     boolean existsByCompanyIdAndDate(Long companyId, LocalDateTime date);
+
+    @Modifying
+    @Query("delete from DIVIDEND d where d.companyId = :companyId")
+    void deleteAllByCompanyId(@Param("companyId") Long companyId);
 }
