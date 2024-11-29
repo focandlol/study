9712e6254e48538bc31678055eaed545ee2bee7d
focandlol <focandlol@naver.com>
2024-11-29 11:25:24 +0900

    basic dp2 Boj11048

---
+++ CodingTest/boj/src/basic/dynamic/dp2/Boj11048.java
@@ -0,0 +1,32 @@
+package basic.dynamic.dp2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj11048 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[n+1][m+1];
+        for(int i=1;i<=n;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=1;j<=m;j++){
+                dp[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                dp[i][j] = Math.max(dp[i-1][j] + dp[i][j],Math.max(dp[i][j-1] + dp[i][j],dp[i-1][j-1] + dp[i][j]));
+            }
+        }
+
+        System.out.println(dp[n][m]);
+    }
+}
