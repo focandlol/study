8e3fe5ef3e532ef5a2d35caea4eb792ab383b8cf
focandlol <focandlol@naver.com>
2024-11-29 16:26:09 +0900

    setting

---
+++ Spring/fintech/domain/src/main/kotlin/focandlol/domain/repository/UserInfoRepository.kt
@@ -0,0 +1,8 @@
+package focandlol.domain.repository
+
+import focandlol.domain.domain.UserInfo
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserInfoRepository : JpaRepository<UserInfo, Long> {
+    fun findByUserKey(userKey: String): UserInfo
+}
