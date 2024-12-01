06609de2bc76c769ef092ada1a224636eaef6d66
focandlol <focandlol@naver.com>
2024-12-01 14:30:29 +0900

    consumer

--- Spring/fintech/consumer/src/main/kotlin/focandlol/consumer/config/JpaAuditingConfiguration.kt
+++ Spring/fintech/consumer/src/main/kotlin/focandlol/consumer/config/JpaAuditingConfiguration.kt
@@ -1,4 +1,4 @@
-package focandlol.api.config
+package focandlol.consumer.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
