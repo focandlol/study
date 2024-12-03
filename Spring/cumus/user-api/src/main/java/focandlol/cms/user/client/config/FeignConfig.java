c4282d723480623be18e13ad3c16a745cfb8198d
focandlol <focandlol@naver.com>
2024-12-03 15:32:46 +0900

    mailgun

---
+++ Spring/cumus/user-api/src/main/java/focandlol/cms/user/client/config/FeignConfig.java
@@ -0,0 +1,16 @@
+package focandlol.cms.user.client.config;
+
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+
+    @Qualifier(value = "mailgun")
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+        return new BasicAuthRequestInterceptor("api", "cf542987399fdb0663de3c15885436dd-f55d7446-95508a20");
+    }
+}
