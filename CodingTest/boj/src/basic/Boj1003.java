a7267b968c93d802dbf112ee0c47d6e09ec99b00
focandlol <focandlol@naver.com>
2024-12-03 19:23:29 +0900

    re

--- CodingTest/boj/src/basic/Boj1003.java
+++ CodingTest/boj/src/basic/Boj1003.java
@@ -15,7 +15,6 @@ public class Boj1003 {
         dp0[1] = 0;
         dp1[0] = 0;
         dp1[1] = 1;
-
         for(int i=2; i<=40; i++){
             dp0[i] = dp0[i-1] + dp0[i-2];
             dp1[i] = dp1[i-1] + dp1[i-2];
