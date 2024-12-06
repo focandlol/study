3b6312fb1dd025b5dfa0886ec92b58db7f3eb72f
focandlol <focandlol@naver.com>
2024-12-06 13:24:05 +0900

    signup

---
+++ Spring/cumus/zerobase-domain/src/main/java/focandlol/domain/common/UserVo.java
@@ -0,0 +1,12 @@
+package focandlol.domain.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserVo {
+
+    private Long id;
+    private String email;
+}
