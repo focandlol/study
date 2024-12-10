672e7eef1b3d136a0247c973ca18d5c1c99c6f7d
focandlol <focandlol@naver.com>
2024-12-10 12:34:13 +0900

    Boj17626

--- Spring/Money/docker/fintech-redis.sh
+++
@@ -1,4 +0,0 @@
-docker run --name money-redis \
-             -p 6379:6379 \
-             --network docker_money \
-             -d redis:latest
--- Spring/Money/domain/src/main/java/focandlol/annotation/Encrypt.java
+++
@@ -1,9 +0,0 @@
-package focandlol.annotation;
-
-import java.lang.annotation.*;
-
-@Target({ElementType.FIELD})
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface Encrypt {
-}
