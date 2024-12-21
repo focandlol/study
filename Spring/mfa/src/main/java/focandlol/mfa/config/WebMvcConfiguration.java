3b8e6a69fb236cff947cfe20f7c5fae4a4f258db
focandlol <focandlol@naver.com>
2024-12-21 18:35:42 +0900

    config

---
+++ Spring/mfa/src/main/java/focandlol/mfa/config/WebMvcConfiguration.java
@@ -0,0 +1,35 @@
+package focandlol.mfa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Bean(name="jsonView")
+    public MappingJackson2JsonView mappingJackson2JsonView() {
+        MappingJackson2JsonView view = new MappingJackson2JsonView();
+        view.setContentType("application/json;charset=UTF-8");
+        return view;
+    }
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/");
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        WebMvcConfigurer.super.configureMessageConverters(converters);
+    }
+}
