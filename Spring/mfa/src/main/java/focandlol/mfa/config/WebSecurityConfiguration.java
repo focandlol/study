c391bb36a5dfb83998cf104fe7610548ed614a24
focandlol <focandlol@naver.com>
2024-12-21 18:35:42 +0900

    config

---
+++ Spring/mfa/src/main/java/focandlol/mfa/config/WebSecurityConfiguration.java
@@ -0,0 +1,35 @@
+package focandlol.mfa.config;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .mvcMatchers("/i18n/**")
+                .mvcMatchers("/resources/**");
+    }
+
+    private String permitallUrl = "/login,/,/prelogin,/mfator,/purelogin";
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.cors().and().csrf().disable();
+
+        http.headers().frameOptions().sameOrigin()
+                .and().authorizeRequests().antMatchers(permitallUrl.split(",")).permitAll()
+                .and().formLogin().loginPage("/login")
+                .and().logout().logoutUrl("/logout").invalidateHttpSession(false).permitAll()
+                .and().authorizeRequests().anyRequest().authenticated();
+    }
+}
