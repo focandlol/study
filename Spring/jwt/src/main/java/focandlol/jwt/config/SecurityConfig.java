ad3d0ea443992a990e8d2aa8f607f542393a03f4
focandlol <focandlol@naver.com>
2024-12-23 20:07:32 +0900

    basic config

---
+++ Spring/jwt/src/main/java/focandlol/jwt/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package focandlol.jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(auth -> auth.disable());
+        http.formLogin(auth -> auth.disable());
+        http.httpBasic(auth -> auth.disable());
+
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/login","/", "/join").permitAll()
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated());
+
+        http.sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
