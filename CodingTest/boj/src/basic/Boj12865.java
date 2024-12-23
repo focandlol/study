4987b16a8d7725663a88e8f4983224bf193a63fd
focandlol <focandlol@naver.com>
2024-12-23 12:57:41 +0900

    Boj12865

---
+++ CodingTest/boj/src/basic/Boj12865.java
@@ -0,0 +1,29 @@
+package basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj12865 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] dp = new int[k+1];
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            for(int j=k; j>=w; j--){
+                dp[j] = Math.max(dp[j],dp[j-w] + v);
+            }
+        }
+
+        System.out.println(dp[k]);
+    }
+}
