dd78c225e011c349816585c3acac6524016849f2
focandlol <focandlol@naver.com>
2025-01-16 18:15:03 +0900

    oauth2Service

---
+++ Spring/Oauth2/src/main/java/focandlol/oauth2/controller/MainController.java
@@ -0,0 +1,13 @@
+package focandlol.oauth2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String mainAPI(){
+        return "main route";
+    }
+}
---
+++ Spring/Oauth2/src/main/java/focandlol/oauth2/dto/UserDto.java
@@ -0,0 +1,18 @@
+package focandlol.oauth2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDto {
+
+    private String role;
+    private String name;
+    private String username;
+
+}
