76dcf3a9ffd67b348404c9c78106a963e2fc9500
focandlol <focandlol@naver.com>
2025-01-24 12:02:42 +0900

    openapi3

--- Spring/doc/src/test/java/focandlol/doc/UserApiDocumentation.java
+++ Spring/doc/src/test/java/focandlol/doc/UserApiDocumentation.java
@@ -1,7 +1,12 @@
 package focandlol.doc;
 
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
@@ -9,6 +14,7 @@ import static org.springframework.restdocs.request.RequestDocumentation.pathPara
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -45,5 +51,23 @@ public class UserApiDocumentation {
                 fieldWithPath("updatedAt").description("The date the user was updated")
             )
         ));
+
+    mockMvc.perform(get("/{id}", 1L))
+        .andDo(document("get-user",
+            preprocessRequest(prettyPrint()),
+            preprocessResponse(prettyPrint()),
+            resource(ResourceSnippetParameters.builder()
+                .description("사용자의 정보를 생성/조회/수정/삭제 합니다.")
+                .summary("사용자 정보")
+                .pathParameters( // 경로 변수 문서화
+                    parameterWithName("id").description("The ID of the user to retrieve"))
+                .responseFields( // 응답 필드 문서화
+                    fieldWithPath("id").description("The unique ID of the user"),
+                    fieldWithPath("phoneNumber").description("The phoneNumber of the user"),
+                    fieldWithPath("email").description("The email of the user"),
+                    fieldWithPath("account").description("The account of the user"),
+                    fieldWithPath("createdAt").description("The date the user was created"),
+                    fieldWithPath("updatedAt").description("The date the user was updated"))
+                .build())));
   }
 }
