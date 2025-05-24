5f00943f2e3185408b15afe56b1b84108c1c6d39
focandlol <focandlol@naver.com>
2025-05-24 20:01:07 +0900

    refactor:
    createProduct es 색인 코드 추가
    deleteProduct es 삭제 코드 추가

--- elastic/elastic/src/main/java/focandlol/elastic/service/ProductService.java
+++ elastic/elastic/src/main/java/focandlol/elastic/service/ProductService.java
@@ -2,7 +2,9 @@ package focandlol.elastic.service;
 
 
 import focandlol.elastic.domain.Product;
+import focandlol.elastic.domain.ProductDocument;
 import focandlol.elastic.dto.CreateProductRequestDto;
+import focandlol.elastic.repository.ProductDocumentRepository;
 import focandlol.elastic.repository.ProductRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Service;
 public class ProductService {
 
   private final ProductRepository productRepository;
+  private final ProductDocumentRepository productDocumentRepository;
 
   public List<Product> getProducts(int page, int size) {
     Pageable pageable = PageRequest.of(page - 1, size);
@@ -22,10 +25,17 @@ public class ProductService {
   }
 
   public Product createProduct(CreateProductRequestDto createProductRequestDto) {
-    return productRepository.save(Product.fromCreateProductRequestDto(createProductRequestDto));
+    Product save = productRepository.save(
+        Product.fromCreateProductRequestDto(createProductRequestDto));
+
+    productDocumentRepository.save(ProductDocument.fromEntity(save));
+
+    return save;
   }
 
   public void deleteProduct(Long id) {
     productRepository.deleteById(id);
+
+    productDocumentRepository.deleteById(id.toString());
   }
 }
